45fad04b620704c58eec5cd7ac006ff864be19f7
wumpz <t.warneke@gmx.net>
2013-05-03 22:36:31 +0200

    added simple delete sql check

--- src/test/java/net/sf/jsqlparser/test/delete/DeleteTest.java
+++ src/test/java/net/sf/jsqlparser/test/delete/DeleteTest.java
@@ -1,11 +1,14 @@
 package net.sf.jsqlparser.test.delete;
 
 import java.io.StringReader;
+import static junit.framework.Assert.assertEquals;
 
 import junit.framework.TestCase;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserManager;
+import net.sf.jsqlparser.statement.Statement;
 import net.sf.jsqlparser.statement.delete.Delete;
+import net.sf.jsqlparser.util.deparser.StatementDeParser;
 
 public class DeleteTest extends TestCase {
 
@@ -21,5 +24,23 @@ public class DeleteTest extends TestCase {
 		Delete delete = (Delete) parserManager.parse(new StringReader(statement));
 		assertEquals("mytable", delete.getTable().getName());
 		assertEquals(statement, "" + delete);
+	}
+	
+	public void testDeleteWhereProblem1() throws JSQLParserException {
+		String stmt = "DELETE FROM tablename WHERE a = 1 AND b = 1";
+		assertSqlCanBeParsedAndDeparsed(stmt);
+	}
+	
+	private void assertSqlCanBeParsedAndDeparsed(String statement) throws JSQLParserException {
+		Statement parsed = parserManager.parse(new StringReader(statement));
+		assertStatementCanBeDeparsedAs(parsed, statement);
+	}
+
+	private void assertStatementCanBeDeparsedAs(Statement parsed, String statement) {
+		assertEquals(statement, parsed.toString());
+
+		StatementDeParser deParser = new StatementDeParser(new StringBuilder());
+		parsed.accept(deParser);
+		assertEquals(statement, deParser.getBuffer().toString());
 	}
 }
