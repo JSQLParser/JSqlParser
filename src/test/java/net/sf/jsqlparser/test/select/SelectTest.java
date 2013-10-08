a0b21c36e5d20088ee713dfe3d020f5a18b58fa3
wumpz <t.warneke@gmx.net>
2013-10-09 00:23:30 +0200

    merge oracle hierarchical syntax into main

--- src/test/java/net/sf/jsqlparser/test/select/SelectTest.java
+++ src/test/java/net/sf/jsqlparser/test/select/SelectTest.java
@@ -1129,6 +1129,11 @@ public class SelectTest extends TestCase {
         assertEquals("param3", namedParameter3.getName());
     }
 	
+	public void testComplexUnion1() throws IOException, JSQLParserException {
+		String stmt = "(SELECT 'abc-' || coalesce(mytab.a::varchar, '') AS a, mytab.b, mytab.c AS st, mytab.d, mytab.e FROM mytab WHERE mytab.del = 0) UNION (SELECT 'cde-' || coalesce(mytab2.a::varchar, '') AS a, mytab2.b, mytab2.bezeichnung AS c, 0 AS d, 0 AS e FROM mytab2 WHERE mytab2.del = 0)";
+		assertSqlCanBeParsedAndDeparsed(stmt);
+	}
+	
 	public void testOracleHierarchicalQuery() throws JSQLParserException {
 		String stmt= "SELECT last_name, employee_id, manager_id FROM employees CONNECT BY employee_id = manager_id ORDER BY last_name";
 		assertSqlCanBeParsedAndDeparsed(stmt);
