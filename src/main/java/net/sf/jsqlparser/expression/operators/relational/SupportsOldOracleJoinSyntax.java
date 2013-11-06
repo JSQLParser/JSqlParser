0528ecca9768cf7a950f470edb212c0e29331da1
wumpz <t.warneke@gmx.net>
2013-11-06 22:11:27 +0100

    simple modifier cleanup

--- src/main/java/net/sf/jsqlparser/expression/operators/relational/SupportsOldOracleJoinSyntax.java
+++ src/main/java/net/sf/jsqlparser/expression/operators/relational/SupportsOldOracleJoinSyntax.java
@@ -23,17 +23,17 @@ package net.sf.jsqlparser.expression.operators.relational;
 
 public interface SupportsOldOracleJoinSyntax {
 
-	final int NO_ORACLE_JOIN = 0;
-	final int ORACLE_JOIN_RIGHT = 1;
-	final int ORACLE_JOIN_LEFT = 2;
+	int NO_ORACLE_JOIN = 0;
+	int ORACLE_JOIN_RIGHT = 1;
+	int ORACLE_JOIN_LEFT = 2;
 
 	int getOldOracleJoinSyntax();
 
 	void setOldOracleJoinSyntax(int oldOracleJoinSyntax);
 
-	final int NO_ORACLE_PRIOR = 0;
-	final int ORACLE_PRIOR_START = 1;
-	final int ORACLE_PRIOR_END = 2;
+	int NO_ORACLE_PRIOR = 0;
+	int ORACLE_PRIOR_START = 1;
+	int ORACLE_PRIOR_END = 2;
 
 	int getOraclePriorPosition();
 
