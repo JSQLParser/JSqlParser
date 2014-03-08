dcd97c16a007c317b822a48bae70c08dc273f2fa
wumpz <t.warneke@gmx.net>
2014-03-08 01:00:29 +0100

    pivot test sqls a little tweaked so parseing deparsing will work

--- src/test/resources/net/sf/jsqlparser/test/oracle-tests/pivot07.sql
+++ src/test/resources/net/sf/jsqlparser/test/oracle-tests/pivot07.sql
@@ -1,5 +1,5 @@
 select *
 from   (select customer_id, product_code, quantity
         from   pivot_test)
-pivot  (sum(quantity) as sum_quantity for (product_code) in ('a' as a, 'b' as b, 'c' as c))
+pivot  (sum(quantity) as sum_quantity for product_code in ('a' as a, 'b' as b, 'c' as c))
 order by customer_id
