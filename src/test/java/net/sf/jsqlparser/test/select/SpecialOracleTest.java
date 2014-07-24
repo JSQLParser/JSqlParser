bbf360e5620b073fe2e1141ef3d7f8db292c0707
wumpz <t.warneke@gmx.net>
2014-07-24 22:04:01 +0200

    recent changes made more oracle tests succeed

--- src/test/java/net/sf/jsqlparser/test/select/SpecialOracleTest.java
+++ src/test/java/net/sf/jsqlparser/test/select/SpecialOracleTest.java
@@ -68,7 +68,7 @@ public class SpecialOracleTest {
         }
         
         LOG.log(Level.INFO, "tested {0} files. got {1} correct parse results", new Object[]{count, success});
-        assertTrue(success>100);
+        assertTrue(success>=106);
     }
 
     @Test
