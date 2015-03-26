8438bdfd6ef54fd9c01d1b09e85218618ae24099
Ferenc Karsany <karsany@karsany.hu>
2015-03-26 01:18:38 +0100

    oracle join (+) vs. ( + ) fix #117

--- src/test/java/net/sf/jsqlparser/statement/StatementsTest.java
+++ src/test/java/net/sf/jsqlparser/statement/StatementsTest.java
@@ -3,34 +3,31 @@ package net.sf.jsqlparser.statement;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.select.Select;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
- *
  * @author toben
  */
 public class StatementsTest {
-    
+
     public StatementsTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -42,11 +39,24 @@ public class StatementsTest {
     public void testStatements() throws JSQLParserException {
         String sqls = "select * from mytable; select * from mytable2;";
         Statements parseStatements = CCJSqlParserUtil.parseStatements(sqls);
-        
+
         assertEquals("SELECT * FROM mytable;\nSELECT * FROM mytable2;\n", parseStatements.toString());
-        
+
         assertTrue(parseStatements.getStatements().get(0) instanceof Select);
         assertTrue(parseStatements.getStatements().get(1) instanceof Select);
     }
-    
+
+    @Test
+    public void testOracleOldJoin() throws JSQLParserException {
+        String sql = "        Select *\n" +
+                "          From dual d1,\n" +
+                "               dual d2\n" +
+                "         Where d1.dummy = d2.dummy ( + );";
+
+
+        Statement parse = CCJSqlParserUtil.parse(sql);
+        assertTrue(parse instanceof Select);
+
+    }
+
 }
