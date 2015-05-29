d66a93a4066ccff0f2603ae54b8c470cc08e5506
wumpz <t.warneke@gmx.net>
2015-05-30 00:49:13 +0200

    astnodes for columns and tables

---
+++ src/test/java/net/sf/jsqlparser/test/select/SelectASTTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2015 JSQLParser.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package net.sf.jsqlparser.test.select;
+
+import junit.framework.TestCase;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.parser.SimpleNode;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.OrderByElement;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectExpressionItem;
+import net.sf.jsqlparser.statement.select.SelectItem;
+
+/**
+ *
+ * @author toben
+ */
+public class SelectASTTest extends TestCase {
+
+    public SelectASTTest(String name) {
+        super(name);
+    }
+    
+    public void testSelectASTColumn() throws JSQLParserException {
+        String sql = "SELECT  a,  b FROM  mytable  order by   b,  c";
+        StringBuilder b = new StringBuilder(sql);
+        Statement stmt = CCJSqlParserUtil.parse(sql);
+        Select select = (Select)stmt;
+        PlainSelect ps = (PlainSelect)select.getSelectBody();
+        for (SelectItem item : ps.getSelectItems()) {
+            SelectExpressionItem sei = (SelectExpressionItem)item;
+            Column c = (Column)sei.getExpression();
+            SimpleNode astNode = c.getASTNode();
+            assertNotNull(astNode);
+            b.setCharAt(astNode.jjtGetFirstToken().beginColumn-1, '*');
+        }
+        for (OrderByElement item : ps.getOrderByElements()) {
+            Column c = (Column)item.getExpression();
+            SimpleNode astNode = c.getASTNode();
+            assertNotNull(astNode);
+            b.setCharAt(astNode.jjtGetFirstToken().beginColumn-1, '#');
+        }
+        assertEquals("SELECT  *,  * FROM  mytable  order by   #,  #", b.toString());
+    }
+}
