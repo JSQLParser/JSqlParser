9e77b6bd55ce24217c5af909b3bde5cb6953ca76
Matthias Bläsing <mblaesing@doppel-helix.eu>
2015-08-02 16:03:23 +0200

    Add support for variable support to "SELECT SKIP <ROWCOUNT> FIRST <ROWCOUNT>..." construct
    The grammar for the construct in informix [1] mentions the possibility, that <ROWCOUNT> can be either
    an integer or a host variable or local SPL variable storing the value of max. The case for plain integers and
    jdbc variables is covered by the first commit While this commit adds support for constructs using SPL
    variables. SPL variables must follow identifier rules [2][3].
    
    [1] http://www-01.ibm.com/support/knowledgecenter/SSGU8G_12.1.0/com.ibm.sqls.doc/ids_sqs_0156.htm
    [2] http://www-01.ibm.com/support/knowledgecenter/SSGU8G_12.1.0/com.ibm.sqls.doc/ids_sqs_1306.htm?lang=de
    [3] http://www-01.ibm.com/support/knowledgecenter/SSGU8G_12.1.0/com.ibm.sqls.doc/ids_sqs_1660.htm%23ids_sqs_1660?lang=de

--- src/main/java/net/sf/jsqlparser/statement/select/First.java
+++ src/main/java/net/sf/jsqlparser/statement/select/First.java
@@ -39,6 +39,7 @@ public class First {
     private Keyword keyword;
     private Long rowCount;
     private JdbcParameter jdbcParameter;
+    private String variable;
 
     public Long getRowCount() {
         return rowCount;
@@ -64,11 +65,25 @@ public class First {
         this.keyword = keyword;
     }
     
+    public String getVariable() {
+        return variable;
+    }
+
+    public void setVariable(String variable) {
+        this.variable = variable;
+    }
+    
     @Override
     public String toString() {
         String result = keyword.name() + " ";
 
-        result += jdbcParameter != null ? jdbcParameter.toString() : rowCount;
+        if(rowCount != null) {
+            result += rowCount;
+        } else if (jdbcParameter != null) {
+            result += jdbcParameter.toString();
+        } else if (variable != null){
+            result += variable;
+        }
 
         return result;
     }
