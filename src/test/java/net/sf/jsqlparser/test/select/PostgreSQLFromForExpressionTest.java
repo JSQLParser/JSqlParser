8d7d8be7fb2c09b1e9c83a92253fc6ddb5420618
ChrissW-R1 <contact@ChrissWR1.me>
2016-06-01 11:19:47 +0200

    -- added unit tests of new SUBSTRING function

---
+++ src/test/java/net/sf/jsqlparser/test/select/PostgreSQLFromForExpressionTest.java
@@ -0,0 +1,43 @@
+package net.sf.jsqlparser.test.select;
+
+import junit.framework.*;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.*;
+import static net.sf.jsqlparser.test.TestUtils.*;
+
+public class PostgreSQLFromForExpressionTest extends TestCase {
+    public PostgreSQLFromForExpressionTest(String arg0) {
+        super(arg0);
+    }
+    
+    public void testInWhere() throws Exception {
+        final String query = "SELECT foo FROM bar WHERE SUBSTRING(foo FROM 1 FOR 10) LIKE 'A%'";
+        assertSqlCanBeParsedAndDeparsed(query);
+    }
+    
+    public void testInOrderBy() throws Exception {
+        final String query = "SELECT foo FROM bar ORDER BY SUBSTRING(foo FROM 12 FOR 2)";
+        assertSqlCanBeParsedAndDeparsed(query);
+    }
+    
+    public void testWithString() throws Exception {
+        final String query = "SELECT SUBSTRING('free text to split' FROM 6 FOR 4) AS foo FROM bar";
+        assertSqlCanBeParsedAndDeparsed(query);
+    }
+    
+    public void testWithToSyntax() throws Exception {
+        final String query = "SELECT SUBSTRING(foo FROM 6 TO 7) AS sub FROM bar";
+        
+        try {
+        	assertSqlCanBeParsedAndDeparsed(query);
+        	
+        	fail("Function SUBSTRING must reject wrong syntax with '... FROM ... TO ...'");
+        } catch (JSQLParserException e) {
+        }
+    }
+    
+    public void testCommaSyntax() throws Exception {
+        final String query = "SELECT SUBSTRING(foo, 6, 4) AS sub FROM bar";
+        assertSqlCanBeParsedAndDeparsed(query);
+    }
+}
