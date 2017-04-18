13922191b006d20d3b848f8af2bf4518bffd997c
Joey Mart <joey.mart@service-now.com>
2017-04-17 18:04:36 -0700

    Fix checkstyle errors -Converted indent tabs to spaces -Added missing {} on single-line if statement

--- src/main/java/net/sf/jsqlparser/expression/MySQLIndexHint.java
+++ src/main/java/net/sf/jsqlparser/expression/MySQLIndexHint.java
@@ -25,27 +25,28 @@ import java.util.List;
 
 public class MySQLIndexHint  {
 
-	private final String action;
-	private final String indexQualifier;
-	private final List<String> indexNames;
+    private final String action;
+    private final String indexQualifier;
+    private final List<String> indexNames;
 
-	public MySQLIndexHint(String action, String indexQualifier, List<String> indexNames) {
-		this.action = action;
-		this.indexQualifier = indexQualifier;
-		this.indexNames = indexNames;
-	}
+    public MySQLIndexHint(String action, String indexQualifier, List<String> indexNames) {
+        this.action = action;
+        this.indexQualifier = indexQualifier;
+        this.indexNames = indexNames;
+    }
 
-	@Override
-	public String toString() {
-		// use|ignore|force key|index (index1,...,indexN)
-		StringBuilder buffer = new StringBuilder();
-		buffer.append(" ").append(action).append(" ").append(indexQualifier).append(" (");
-		for (int i = 0; i < indexNames.size(); i++) {
-			if (i > 0)
-				buffer.append(",");
-			buffer.append(indexNames.get(i));
-		}
-		buffer.append(")");
-		return buffer.toString();
-	}
+    @Override
+    public String toString() {
+        // use|ignore|force key|index (index1,...,indexN)
+        StringBuilder buffer = new StringBuilder();
+        buffer.append(" ").append(action).append(" ").append(indexQualifier).append(" (");
+        for (int i = 0; i < indexNames.size(); i++) {
+            if (i > 0) {
+                buffer.append(",");
+            }
+            buffer.append(indexNames.get(i));
+        }
+        buffer.append(")");
+        return buffer.toString();
+    }
 }
