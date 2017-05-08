84e3414153f8c796a4bace148d81df2f6cc1fcb4
Christian Cheng <Christian Cheng>
2017-05-08 20:08:30 +0800

    Prefer the first style of testing
    As discussed in issue #442

--- src/test/java/net/sf/jsqlparser/util/deparser/StatementDeParserTest.java
+++ src/test/java/net/sf/jsqlparser/util/deparser/StatementDeParserTest.java
@@ -1,30 +1,30 @@
 package net.sf.jsqlparser.util.deparser;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasToString;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
-import static org.mockito.hamcrest.MockitoHamcrest.argThat;
+import static org.mockito.Mockito.spy;
 
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.LongValue;
-import net.sf.jsqlparser.parser.CCJSqlParserManager;
-import net.sf.jsqlparser.parser.JSqlParser;
+import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.delete.Delete;
 import net.sf.jsqlparser.statement.insert.Insert;
 import net.sf.jsqlparser.statement.select.OrderByElement;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectBody;
+import net.sf.jsqlparser.statement.select.WithItem;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StatementDeParserTest {
@@ -67,16 +67,56 @@ public class StatementDeParserTest {
         then(orderByElement2Expression).should().accept(expressionDeParser);
     }
 
-    private JSqlParser jSqlParser = new CCJSqlParserManager();
-
     @Test
     public void shouldUseProvidedExpressionDeparserWhenDeParsingInsert() throws JSQLParserException {
-        Insert insert = (Insert) jSqlParser.parse(new StringReader("INSERT INTO mytable (col1) VALUES (123), (456) ON DUPLICATE KEY UPDATE col3 = 789")); 
+        Insert insert = new Insert();
+        Table table = new Table();
+        List<Column> duplicateUpdateColumns = new ArrayList<Column>();
+        List<Expression> duplicateUpdateExpressionList = new ArrayList<Expression>();
+        Column duplicateUpdateColumn1 = new Column();
+        Column duplicateUpdateColumn2 = new Column();
+        Expression duplicateUpdateExpression1 = mock(Expression.class);
+        Expression duplicateUpdateExpression2 = mock(Expression.class);
+        Select select = new Select();
+        List<WithItem> withItemsList = new ArrayList<WithItem>();
+        WithItem withItem1 = spy(new WithItem());
+        WithItem withItem2 = spy(new WithItem());
+        SelectBody withItem1SelectBody = mock(SelectBody.class);
+        SelectBody withItem2SelectBody = mock(SelectBody.class);
+        SelectBody selectBody = mock(SelectBody.class);
+
+        insert.setSelect(select);
+        insert.setTable(table);
+        insert.setUseDuplicate(true);
+        insert.setDuplicateUpdateColumns(duplicateUpdateColumns);
+        insert.setDuplicateUpdateExpressionList(duplicateUpdateExpressionList);
+        duplicateUpdateColumns.add(duplicateUpdateColumn1);
+        duplicateUpdateColumns.add(duplicateUpdateColumn2);
+        duplicateUpdateExpressionList.add(duplicateUpdateExpression1);
+        duplicateUpdateExpressionList.add(duplicateUpdateExpression2);
+        insert.setDuplicateUpdateExpressionList(duplicateUpdateExpressionList);
+        select.setWithItemsList(withItemsList);
+        select.setSelectBody(selectBody);
+        withItemsList.add(withItem1);
+        withItemsList.add(withItem2);
+        withItem1.setSelectBody(withItem1SelectBody);
+        withItem2.setSelectBody(withItem2SelectBody);
 
         statementDeParser.visit(insert);
 
-        then(expressionDeParser).should().visit((LongValue) argThat(hasToString(equalTo("123"))));
-        then(expressionDeParser).should().visit((LongValue) argThat(hasToString(equalTo("456"))));
-        then(expressionDeParser).should().visit((LongValue) argThat(hasToString(equalTo("789"))));
+        then(withItem1).should().accept(argThat(isSelectDeParserWithExpressionDeParser(expressionDeParser)));
+        then(withItem2).should().accept(argThat(isSelectDeParserWithExpressionDeParser(expressionDeParser)));
+        then(selectBody).should().accept(argThat(isSelectDeParserWithExpressionDeParser(expressionDeParser)));
+        then(duplicateUpdateExpression1).should().accept(expressionDeParser);
+        then(duplicateUpdateExpression1).should().accept(expressionDeParser);
+    }
+
+    private ArgumentMatcher<SelectDeParser> isSelectDeParserWithExpressionDeParser(final ExpressionDeParser expressionDeParser) {
+        return new ArgumentMatcher<SelectDeParser>() {
+            @Override
+            public boolean matches(SelectDeParser argument) {
+                return argument.getExpressionVisitor().equals(expressionDeParser);
+            }
+        };
     }
 }
