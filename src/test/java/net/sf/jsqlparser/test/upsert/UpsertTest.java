4a1248535edfc4da7dfb5a3a62816b6a6fb08dd2
messfish <messfish@users.noreply.github.com>
2017-06-05 18:38:42 -0400

    Add files via upload

--- src/test/java/net/sf/jsqlparser/test/upsert/UpsertTest.java
+++ src/test/java/net/sf/jsqlparser/test/upsert/UpsertTest.java
@@ -7,7 +7,13 @@ import java.io.StringReader;
 import org.junit.Test;
 
 import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.LongValue;
+import net.sf.jsqlparser.expression.StringValue;
+import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
 import net.sf.jsqlparser.parser.CCJSqlParserManager;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.upsert.Upsert;
 
 public class UpsertTest {
@@ -16,36 +22,71 @@ public class UpsertTest {
 
     @Test
     public void testUpsert() throws JSQLParserException {
-        String sql="UPSERT INTO TEST(NAME,ID) VALUES('foo',123);";
-        Upsert upsert= (Upsert)parserManager.parse(new StringReader(sql));
-        System.out.println(upsert.getColumns());
-        System.out.println(upsert.getTable().getName());
-        System.out.println(upsert.getItemsList() );
-        System.out.println(upsert.toString());
+        String statement ="UPSERT INTO TEST (NAME, ID) VALUES ('foo', 123)";
+        Upsert upsert = (Upsert)parserManager.parse(new StringReader(statement));
+        assertEquals("TEST", upsert.getTable().getName());
+        assertEquals(2, upsert.getColumns().size());
+        assertEquals("NAME", ((Column) upsert.getColumns().get(0)).getColumnName());
+        assertEquals("ID", ((Column) upsert.getColumns().get(1)).getColumnName());
+        assertEquals(2, ((ExpressionList) upsert.getItemsList()).getExpressions().size());
+        assertEquals("foo",
+                ((StringValue) ((ExpressionList) upsert.getItemsList()).getExpressions().get(0)).
+                        getValue());
+        assertEquals(123, ((LongValue) ((ExpressionList) upsert.getItemsList()).getExpressions().
+                get(1)).getValue());
+        assertEquals(statement, "" + upsert);
     }
 
     @Test
     public void testUpsertDuplicate() throws JSQLParserException {
-        String sql="UPSERT INTO TEST(ID, COUNTER) VALUES(123, 0) ON DUPLICATE KEY UPDATE COUNTER = COUNTER + 1;";
-        Upsert upsert= (Upsert)parserManager.parse(new StringReader(sql));
-        System.out.println(upsert.getDuplicateUpdateColumns()+" "+upsert.getDuplicateUpdateExpressionList());
+        String statement="UPSERT INTO TEST (ID, COUNTER) VALUES (123, 0) ON DUPLICATE KEY UPDATE COUNTER = COUNTER + 1";
+        Upsert upsert= (Upsert)parserManager.parse(new StringReader(statement));
+        assertEquals("TEST", upsert.getTable().getName());
+        assertEquals(2, upsert.getColumns().size());
+        assertEquals("ID", ((Column) upsert.getColumns().get(0)).getColumnName());
+        assertEquals("COUNTER", ((Column) upsert.getColumns().get(1)).getColumnName());
+        assertEquals(2, ((ExpressionList) upsert.getItemsList()).getExpressions().size());
+        assertEquals(123, ((LongValue) ((ExpressionList) upsert.getItemsList()).getExpressions().
+                get(0)).getValue());
+        assertEquals(0, ((LongValue) ((ExpressionList) upsert.getItemsList()).getExpressions().
+                get(1)).getValue());
+        assertEquals(1, upsert.getDuplicateUpdateColumns().size());
+        assertEquals("COUNTER", ((Column)upsert.getDuplicateUpdateColumns().get(0)).getColumnName());
+        assertEquals(1, upsert.getDuplicateUpdateExpressionList().size());
+        assertEquals("COUNTER + 1", upsert.getDuplicateUpdateExpressionList().get(0).toString());
+        assertEquals(statement, "" + upsert);
     }
 
     @Test
     public void testUpsertSelect() throws JSQLParserException {
-        String sql="UPSERT INTO test.targetTable(col1, col2) SELECT col3, col4 FROM test.sourceTable WHERE col5 < 100";
-        Upsert upsert= (Upsert)parserManager.parse(new StringReader(sql));
-        System.out.println(upsert);
-        System.out.println(upsert.getSelect());
+        String statement="UPSERT INTO test.targetTable (col1, col2) SELECT * FROM test.sourceTable";
+        Upsert upsert= (Upsert)parserManager.parse(new StringReader(statement));
+        assertEquals("test.targetTable", upsert.getTable().getFullyQualifiedName());
+        assertEquals(2, upsert.getColumns().size());
+        assertEquals("col1", ((Column) upsert.getColumns().get(0)).getColumnName());
+        assertEquals("col2", ((Column) upsert.getColumns().get(1)).getColumnName());
+        assertNull(upsert.getItemsList());
+        assertNotNull(upsert.getSelect());
+        assertEquals("test.sourceTable",
+                ((Table) ((PlainSelect) upsert.getSelect().getSelectBody()).getFromItem()).getFullyQualifiedName());
+        assertEquals(statement, "" + upsert);
     }
 
     @Test
     public void testUpsertN() throws JSQLParserException {
-        String sql="UPSERT INTO TEST VALUES('foo','bar',3);";
-        Upsert upsert= (Upsert)parserManager.parse(new StringReader(sql));
-        System.out.println(upsert.getColumns());
-        System.out.println(upsert);
-        System.out.println(upsert.getItemsList());
+        String statement="UPSERT INTO TEST VALUES ('foo', 'bar', 3)";
+        Upsert upsert= (Upsert)parserManager.parse(new StringReader(statement));
+        assertEquals("TEST", upsert.getTable().getName());
+        assertEquals(3, ((ExpressionList) upsert.getItemsList()).getExpressions().size());
+        assertEquals("foo",
+                ((StringValue) ((ExpressionList) upsert.getItemsList()).getExpressions().get(0)).
+                        getValue());
+        assertEquals("bar",
+                ((StringValue) ((ExpressionList) upsert.getItemsList()).getExpressions().get(1)).
+                        getValue());
+        assertEquals(3, ((LongValue) ((ExpressionList) upsert.getItemsList()).getExpressions().
+                get(2)).getValue());
+        assertEquals(statement, "" + upsert);
     }
     
 }
