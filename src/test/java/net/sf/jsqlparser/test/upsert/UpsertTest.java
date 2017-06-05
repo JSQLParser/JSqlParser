904c68830a7a6e75d5a805c31641406ccbd0fb7c
messfish <messfish@users.noreply.github.com>
2017-06-05 16:14:58 -0400

    Create UpsertTest.java

---
+++ src/test/java/net/sf/jsqlparser/test/upsert/UpsertTest.java
@@ -0,0 +1,51 @@
+package net.sf.jsqlparser.test.upsert;
+
+import static org.junit.Assert.*;
+
+import java.io.StringReader;
+
+import org.junit.Test;
+
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserManager;
+import net.sf.jsqlparser.statement.upsert.Upsert;
+
+public class UpsertTest {
+
+    CCJSqlParserManager parserManager = new CCJSqlParserManager();
+
+    @Test
+    public void testUpsert() throws JSQLParserException {
+        String sql="UPSERT INTO TEST(NAME,ID) VALUES('foo',123);";
+        Upsert upsert= (Upsert)parserManager.parse(new StringReader(sql));
+        System.out.println(upsert.getColumns());
+        System.out.println(upsert.getTable().getName());
+        System.out.println(upsert.getItemsList() );
+        System.out.println(upsert.toString());
+    }
+
+    @Test
+    public void testUpsertDuplicate() throws JSQLParserException {
+        String sql="UPSERT INTO TEST(ID, COUNTER) VALUES(123, 0) ON DUPLICATE KEY UPDATE COUNTER = COUNTER + 1;";
+        Upsert upsert= (Upsert)parserManager.parse(new StringReader(sql));
+        System.out.println(upsert.getDuplicateUpdateColumns()+" "+upsert.getDuplicateUpdateExpressionList());
+    }
+
+    @Test
+    public void testUpsertSelect() throws JSQLParserException {
+        String sql="UPSERT INTO test.targetTable(col1, col2) SELECT col3, col4 FROM test.sourceTable WHERE col5 < 100";
+        Upsert upsert= (Upsert)parserManager.parse(new StringReader(sql));
+        System.out.println(upsert);
+        System.out.println(upsert.getSelect());
+    }
+
+    @Test
+    public void testUpsertN() throws JSQLParserException {
+        String sql="UPSERT INTO TEST VALUES('foo','bar',3);";
+        Upsert upsert= (Upsert)parserManager.parse(new StringReader(sql));
+        System.out.println(upsert.getColumns());
+        System.out.println(upsert);
+        System.out.println(upsert.getItemsList());
+    }
+    
+}
