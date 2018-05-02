7442fee5cf50cf433739ae7b699ee546de99834e
sam <sam1287@gmail.com>
2018-05-02 21:51:52 +0900

    Simple fix - If select sql contain ISNULL or isnull parsing always failed.

---
+++ src/test/java/net/sf/jsqlparser/test/select/IsNullFunctionInSelectTest.java
@@ -0,0 +1,62 @@
+/*
+ * #%L
+ * JSQLParser library
+ * %%
+ * Copyright (C) 2004 - 2017 JSQLParser
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+package net.sf.jsqlparser.test.select;
+
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.StatementVisitorAdapter;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectVisitorAdapter;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author sam
+ */
+public class IsNullFunctionInSelectTest {
+    @Test
+    public void sqlContainIsNullFunctionShouldBeParsed() throws JSQLParserException {
+        String sql = "select name, age, ISNULL(home, 'earn more money') from person";
+        Statement statement = CCJSqlParserUtil.parse(sql);
+        assertThat(accept(statement), is(true));
+    }
+
+    private boolean accept(Statement statement) {
+        statement.accept(new StatementVisitorAdapter() {
+            @Override
+            public void visit(Select select) {
+                select.getSelectBody().accept(new SelectVisitorAdapter() {
+                    @Override
+                    public void visit(PlainSelect plainSelect) {
+                        // nothing to do
+                    }
+                });
+            }
+
+        });
+        return true;
+    }
+}
