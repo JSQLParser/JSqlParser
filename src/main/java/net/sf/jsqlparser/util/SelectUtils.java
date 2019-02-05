373c6cb5973409104a161bc0ace95375126483c6
wumpz <t.warneke@gmx.net>
2019-02-06 00:04:45 +0100

    changed license header to represent the projects dual license

--- src/main/java/net/sf/jsqlparser/util/SelectUtils.java
+++ src/main/java/net/sf/jsqlparser/util/SelectUtils.java
@@ -4,19 +4,7 @@
  * %%
  * Copyright (C) 2004 - 2014 JSQLParser
  * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 2.1 of the 
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * Dual licensed under GNU LGPL 2.1 or Apache License 2.0
  * #L%
  */
 package net.sf.jsqlparser.util;
