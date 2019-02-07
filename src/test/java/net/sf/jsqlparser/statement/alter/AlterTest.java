bfb8023318c31b2e424051ece271b5572f47516c
Simon <ksr.jonasson@gmail.com>
2019-02-07 15:49:28 +0100

    Added support for 'ALTER TABLE CHANGE COLUMN' (#741)
    * Added support for 'ALTER TABLE CHANGE COLUMN oldName newName columnDefinition'. Please see https://dev.mysql.com/doc/refman/8.0/en/alter-table.html for reference.
    
    * Returned import ordering to avoid conflicts
    
    * Improved the tests somewhat
    
    Now also test the getOptionalSpecifier() for both cases (null and not-null)
    
    * Expanded tests for ALTER TABLE ... CHANGE

--- src/test/java/net/sf/jsqlparser/statement/alter/AlterTest.java
+++ src/test/java/net/sf/jsqlparser/statement/alter/AlterTest.java
@@ -263,6 +263,34 @@ public class AlterTest {
     }
 
     @Test
+    public void testAlterTableChangeColumn1() throws JSQLParserException {
+        Statement stmt = CCJSqlParserUtil.parse("ALTER TABLE tb_test CHANGE COLUMN c1 c2 INT (10)");
+        Alter alter = (Alter) stmt;
+        assertEquals(AlterOperation.CHANGE, alter.getAlterExpressions().get(0).getOperation());
+        assertEquals("c1", alter.getAlterExpressions().get(0).getColOldName());
+        assertEquals("COLUMN", alter.getAlterExpressions().get(0).getOptionalSpecifier());
+    }
+
+    @Test
+    public void testAlterTableChangeColumn2() throws JSQLParserException {
+        Statement stmt = CCJSqlParserUtil.parse("ALTER TABLE tb_test CHANGE c1 c2 INT (10)");
+        Alter alter = (Alter) stmt;
+        assertEquals(AlterOperation.CHANGE, alter.getAlterExpressions().get(0).getOperation());
+        assertEquals("c1", alter.getAlterExpressions().get(0).getColOldName());
+        assertNull(alter.getAlterExpressions().get(0).getOptionalSpecifier());
+    }
+
+    @Test
+    public void testAlterTableChangeColumn3() throws JSQLParserException {
+        assertSqlCanBeParsedAndDeparsed("ALTER TABLE tb_test CHANGE COLUMN c1 c2 INT (10)");
+    }
+
+    @Test
+    public void testAlterTableChangeColumn4() throws JSQLParserException {
+        assertSqlCanBeParsedAndDeparsed("ALTER TABLE tb_test CHANGE c1 c2 INT (10)");
+    }
+
+    @Test
     public void testAlterTableAddColumnWithZone() throws JSQLParserException {
         assertSqlCanBeParsedAndDeparsed("ALTER TABLE mytable ADD COLUMN col1 timestamp with time zone");
         assertSqlCanBeParsedAndDeparsed("ALTER TABLE mytable ADD COLUMN col1 timestamp without time zone");
