3209a16c55c1976d5b772c607fd4b9d5fb9f9483
wumpz <t.warneke@gmx.net>
2019-02-19 01:35:14 +0100

    fixes #753

--- src/test/java/net/sf/jsqlparser/statement/select/SelectTest.java
+++ src/test/java/net/sf/jsqlparser/statement/select/SelectTest.java
@@ -1064,6 +1064,13 @@ public class SelectTest {
     }
 
     @Test
+    public void testEscapedFunctionsIssue753() throws JSQLParserException {
+        Statement stmt = CCJSqlParserUtil.parse("SELECT { fn test(0)} AS COL");
+        assertEquals("SELECT {fn test(0)} AS COL", stmt.toString());
+        assertSqlCanBeParsedAndDeparsed("SELECT fn FROM fn");
+    }
+
+    @Test
     public void testNamedParametersPR702() throws JSQLParserException {
         assertSqlCanBeParsedAndDeparsed("SELECT substring(id, 2, 3), substring(id from 2 for 3), substring(id from 2), trim(BOTH ' ' from 'foo bar '), trim(LEADING ' ' from 'foo bar '), trim(TRAILING ' ' from 'foo bar '), trim(' ' from 'foo bar '), position('foo' in 'bar'), overlay('foo' placing 'bar' from 1), overlay('foo' placing 'bar' from 1 for 2) FROM my table");
     }
