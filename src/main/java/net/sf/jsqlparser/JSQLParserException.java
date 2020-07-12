25b6035d2b20e3bc36a59a91c1a2ca5ca001a9ef
gitmotte <www@synbee.at>
2020-07-12 19:06:45 +0200

    remove overrides of * getCause * printStackTrace variants
    why add an additional cause ?
    
    set cause.getMessage() the message within constructor
    JSQLParserException(Throwable cause), othewise cause.toString() will be
    set as default.

--- src/main/java/net/sf/jsqlparser/JSQLParserException.java
+++ src/main/java/net/sf/jsqlparser/JSQLParserException.java
@@ -12,50 +12,21 @@ package net.sf.jsqlparser;
 public class JSQLParserException extends Exception {
 
     private static final long serialVersionUID = -1099039459759769980L;
-    private Throwable cause = null;
 
     public JSQLParserException() {
         super();
     }
 
-    public JSQLParserException(String arg0) {
-        super(arg0);
+    public JSQLParserException(String message, Throwable cause) {
+        super(message, cause);
     }
 
-    public JSQLParserException(Throwable arg0) {
-        this.cause = arg0;
+    public JSQLParserException(String message) {
+        super(message);
     }
 
-    public JSQLParserException(String arg0, Throwable arg1) {
-        super(arg0);
-        this.cause = arg1;
+    public JSQLParserException(Throwable cause) {
+        super(cause == null ? null : cause.getMessage(), cause);
     }
 
-    @Override
-    public synchronized Throwable getCause() {
-        return cause;
-    }
-
-    @Override
-    public void printStackTrace() {
-        printStackTrace(System.err);
-    }
-
-    @Override
-    public void printStackTrace(java.io.PrintWriter pw) {
-        super.printStackTrace(pw);
-        if (cause != null) {
-            pw.println("Caused by:");
-            cause.printStackTrace(pw);
-        }
-    }
-
-    @Override
-    public void printStackTrace(java.io.PrintStream ps) {
-        super.printStackTrace(ps);
-        if (cause != null) {
-            ps.println("Caused by:");
-            cause.printStackTrace(ps);
-        }
-    }
 }
