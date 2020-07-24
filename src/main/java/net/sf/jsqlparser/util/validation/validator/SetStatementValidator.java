971bfac50413588dfc34210391347fe275cdc02d
gitmotte <www@synbee.at>
2020-07-24 21:12:19 +0200

    + SetStatementValidator

--- src/main/java/net/sf/jsqlparser/util/validation/validator/SetStatementValidator.java
+++ src/main/java/net/sf/jsqlparser/util/validation/validator/SetStatementValidator.java
@@ -24,20 +24,9 @@ public class SetStatementValidator extends AbstractValidator<SetStatement> {
         for (ValidationCapability c : getCapabilities()) {
             validateFeature(c, Feature.set);
         }
-        //        buffer.append("SET ");
-        //
-        //        for (int i = 0; i < set.getCount(); i++) {
-        //            if (i > 0) {
-        //                buffer.append(", ");
-        //            }
-        //            buffer.append(set.getName(i));
-        //            if (set.isUseEqual(i)) {
-        //                buffer.append(" =");
-        //            }
-        //            buffer.append(" ");
-        //            set.getExpression(i).accept(expressionVisitor);
-        //        }
-
+        for (int i = 0; i < set.getCount(); i++) {
+            validateOptionalExpression(set.getExpression(i));
+        }
     }
 
 }
