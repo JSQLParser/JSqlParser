2074bfb2683b4f78697ac3c4b0ff4ee0c031361c
gitmotte <www@synbee.at>
2020-09-16 11:55:27 +0200

    implement CreateIndexValidatorTest

--- src/test/java/net/sf/jsqlparser/util/validation/validator/CreateIndexValidatorTest.java
+++ src/test/java/net/sf/jsqlparser/util/validation/validator/CreateIndexValidatorTest.java
@@ -1,13 +1,29 @@
 package net.sf.jsqlparser.util.validation.validator;
 
+import java.util.Arrays;
 import org.junit.Test;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.feature.Feature;
 import net.sf.jsqlparser.util.validation.ValidationTestAsserts;
+import net.sf.jsqlparser.util.validation.feature.DatabaseType;
+import net.sf.jsqlparser.util.validation.feature.FeaturesAllowed;
 
 public class CreateIndexValidatorTest extends ValidationTestAsserts {
 
     @Test
-    public void test() {
+    public void testValidateCreateIndex() throws JSQLParserException {
+        for (String sql : Arrays.asList(
+                "CREATE INDEX idx_american_football_action_plays_1 ON american_football_action_plays USING btree (play_type)")) {
+            validateNoErrors(sql, 1, DatabaseType.DATABASES);
+        }
+    }
 
+    @Test
+    public void testValidateCreateIndexNotAllowed() throws JSQLParserException {
+        for (String sql : Arrays.asList(
+                "CREATE INDEX idx_american_football_action_plays_1 ON american_football_action_plays USING btree (play_type)")) {
+            validateNotAllowed(sql, 1, 1, FeaturesAllowed.DML, Feature.createIndex);
+        }
     }
 
 }
