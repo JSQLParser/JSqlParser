7f0de9ca241a701d01e61962539da93a8158b059
gitmotte <www@synbee.at>
2020-09-21 16:17:14 +0200

    add AlterSequenceValidatorTest

--- src/test/java/net/sf/jsqlparser/util/validation/validator/AlterSequenceValidatorTest.java
+++ src/test/java/net/sf/jsqlparser/util/validation/validator/AlterSequenceValidatorTest.java
@@ -9,14 +9,23 @@
  */
 package net.sf.jsqlparser.util.validation.validator;
 
+import java.util.Arrays;
 import org.junit.Test;
+import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.util.validation.ValidationTestAsserts;
+import net.sf.jsqlparser.util.validation.feature.DatabaseType;
 
 public class AlterSequenceValidatorTest extends ValidationTestAsserts {
 
+    private static final DatabaseType DATABASES_SUPPORTING_SEQUENCES[] = new DatabaseType[] { DatabaseType.ORACLE,
+            DatabaseType.SQLSERVER, DatabaseType.MARIADB, DatabaseType.POSTGRESQL, DatabaseType.H2 };
+
     @Test
-    public void test() {
-
+    public void testAlterSequence_noParams() throws JSQLParserException {
+        for (String sql : Arrays.asList("ALTER SEQUENCE my_seq","ALTER SEQUENCE my_seq INCREMENT BY 1","ALTER SEQUENCE my_seq START WITH 10","ALTER SEQUENCE my_seq MAXVALUE 5",
+                "ALTER SEQUENCE my_seq NOMAXVALUE","ALTER SEQUENCE my_seq MINVALUE 5","ALTER SEQUENCE my_seq NOMINVALUE","ALTER SEQUENCE my_seq CYCLE",
+                "ALTER SEQUENCE my_sec INCREMENT BY 2 START WITH 10","ALTER SEQUENCE my_sec START WITH 2 INCREMENT BY 5 NOCACHE","ALTER SEQUENCE my_sec START WITH 2 INCREMENT BY 5 CACHE 200 CYCLE")) { 
+            validateNoErrors(sql, 1, DATABASES_SUPPORTING_SEQUENCES);
+        }
     }
-
 }
