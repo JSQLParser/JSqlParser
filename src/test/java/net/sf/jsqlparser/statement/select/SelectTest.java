c3a1aa688442c1d81d5f807f5243f56e6c73b663
Tomer Shay (Shimshi) <shimshi.tomer@gmail.com>
2021-03-14 22:10:33 +0200

    Add support for union_with_brackets_and_orderby (#1131)

--- src/test/java/net/sf/jsqlparser/statement/select/SelectTest.java
+++ src/test/java/net/sf/jsqlparser/statement/select/SelectTest.java
@@ -2410,6 +2410,12 @@ public class SelectTest {
     }
 
     @Test
+    public void testUnionWithBracketsAndOrderBy() throws JSQLParserException {
+        String stmt = "(SELECT a FROM tbl ORDER BY a) UNION DISTINCT (SELECT a FROM tbl ORDER BY a)";
+        assertSqlCanBeParsedAndDeparsed(stmt);
+    }
+
+    @Test
     public void testMultiValueNotInBinds() throws JSQLParserException {
         String stmt = "SELECT * FROM mytable WHERE (a, b) NOT IN ((?, ?), (?, ?))";
         assertSqlCanBeParsedAndDeparsed(stmt);
