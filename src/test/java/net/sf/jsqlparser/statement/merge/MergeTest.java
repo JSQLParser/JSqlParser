92b64f326dc2d744e8ae24008021fb0ef6e2d834
Andreas Reichel <andreas@manticore-projects.com>
2021-04-12 07:08:35 +0700

    Add WHERE expression to MergeInsert Add test case for MergeInsert WHERE expression

--- src/test/java/net/sf/jsqlparser/statement/merge/MergeTest.java
+++ src/test/java/net/sf/jsqlparser/statement/merge/MergeTest.java
@@ -13,6 +13,7 @@ import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.Statement;
 import static net.sf.jsqlparser.test.TestUtils.*;
+import org.assertj.core.api.Assertions;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 
@@ -151,9 +152,6 @@ public class MergeTest {
                 + "    )\n";
 
         Statement statement = CCJSqlParserUtil.parse(sql);
-
-        System.out.println(statement.toString());
-
         assertSqlCanBeParsedAndDeparsed(sql, true);
     }
 
@@ -195,4 +193,32 @@ public class MergeTest {
        
        //@todo: add a testcase supposed to not finding a misplaced hint
     }
+
+  @Test
+  public void testInsertMergeWhere() throws JSQLParserException {
+    String sql =
+        "-- Both clauses present.\n"
+            + "MERGE INTO test1 a\n"
+            + "  USING all_objects b\n"
+            + "    ON (a.object_id = b.object_id)\n"
+            + "  WHEN MATCHED THEN\n"
+            + "    UPDATE SET a.status = b.status\n"
+            + "    WHERE  b.status != 'VALID'\n"
+            + "  WHEN NOT MATCHED THEN\n"
+            + "    INSERT (object_id, status)\n"
+            + "    VALUES (b.object_id, b.status)\n"
+            + "\n"
+            + "    WHERE  b.status != 'VALID'\n"
+            ;
+
+    Statement statement = CCJSqlParserUtil.parse(sql);
+    assertSqlCanBeParsedAndDeparsed(sql, true);
+    
+    Merge merge = (Merge) statement;
+    MergeInsert mergeInsert = merge.getMergeInsert();
+    Assertions.assertThat( mergeInsert.getWhereCondition() );
+    
+    MergeUpdate mergeUpdate = merge.getMergeUpdate();
+    Assertions.assertThat( mergeUpdate.getWhereCondition() );
+  }
 }
