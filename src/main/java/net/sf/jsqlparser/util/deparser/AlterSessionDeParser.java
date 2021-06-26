3a46a29d69366115065d3013ee41fdf84df6cf75
manticore-projects <andreas@manticore-projects.com>
2021-06-27 05:38:19 +0700

    Implement Oracle Alter Session Statements (#1234)
    * Implement Oracle Alter Session Statements according to https://docs.oracle.com/cd/B19306_01/server.102/b14200/statements_2012.htm
    
    * Implement PMD Rule "SwitchStmtsShouldHaveDefault"
    
    * Reorganize Test Case imports

---
+++ src/main/java/net/sf/jsqlparser/util/deparser/AlterSessionDeParser.java
@@ -0,0 +1,25 @@
+/*-
+ * #%L
+ * JSQLParser library
+ * %%
+ * Copyright (C) 2004 - 2019 JSQLParser
+ * %%
+ * Dual licensed under GNU LGPL 2.1 or Apache License 2.0
+ * #L%
+ */
+package net.sf.jsqlparser.util.deparser;
+
+import net.sf.jsqlparser.statement.alter.AlterSession;
+
+public class AlterSessionDeParser extends AbstractDeParser<AlterSession> {
+
+    public AlterSessionDeParser(StringBuilder buffer) {
+        super(buffer);
+    }
+
+    @Override
+    public void deParse(AlterSession alterSession) {
+        buffer.append(alterSession.toString());
+    }
+
+}
