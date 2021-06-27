f7ea4a5040ac7c7f489dd1164ed581601530ef65
Tobias Warneke <t.warneke@gmx.net>
2021-06-27 02:20:35 +0200

    corrected javadoc problem

--- src/main/java/net/sf/jsqlparser/statement/alter/AlterSession.java
+++ src/main/java/net/sf/jsqlparser/statement/alter/AlterSession.java
@@ -7,21 +7,6 @@
  * Dual licensed under GNU LGPL 2.1 or Apache License 2.0
  * #L%
  */
-/*
- * Copyright (C) 2021 JSQLParser.
- *
- * This library is free software; you can redistribute it and/or modify it under the terms of the
- * GNU Lesser General Public License as published by the Free Software Foundation; either version
- * 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with this library;
- * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA
- */
 
 package net.sf.jsqlparser.statement.alter;
 
@@ -32,7 +17,7 @@ import net.sf.jsqlparser.statement.StatementVisitor;
 /**
  *
  * @author are
- * @@link  https://docs.oracle.com/cd/B19306_01/server.102/b14200/statements_2012.htm
+ * @see  <a href="https://docs.oracle.com/cd/B19306_01/server.102/b14200/statements_2012.htm">ALTER SESSION</a>
  */
 public class AlterSession implements Statement  {
     private AlterSessionOperation operation;
