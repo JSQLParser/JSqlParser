21e5ebac02822e2d5fd3a29fd4baa01b61255dab
manticore-projects <andreas@manticore-projects.com>
2021-08-28 04:37:05 +0700

    Update sets (#1317)
    * Fixes #1306
    
    Nested Cases with Complex Expressions
    
    * Reduce coverage for Java 8
    
    * GROUP BY with Complex Expressions
    
    Fixes #1308
    
    * Update Sets with Complex Expressions
    
    Fixes #1316
    
    * Update Sets with Complex Expressions
    
    Fix existing tests
    Add tests for the new functionality
    
    * Implement PMD/Codacy recommendations

---
+++ src/main/java/net/sf/jsqlparser/statement/update/UpdateSet.java
@@ -0,0 +1,64 @@
+package net.sf.jsqlparser.statement.update;
+
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
+import net.sf.jsqlparser.schema.Column;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class UpdateSet {
+    protected boolean usingBrackets = false;
+    protected ArrayList<Column> columns = new ArrayList<>();
+    protected ArrayList<Expression> expressions = new ArrayList<>();
+
+    public UpdateSet() {
+
+    }
+
+    public UpdateSet(Column column) {
+        this.columns.add(column);
+    }
+
+    public UpdateSet(Column column, Expression expression) {
+        this.columns.add(column);
+        this.expressions.add(expression);
+    }
+
+    public boolean isUsingBrackets() {
+        return usingBrackets;
+    }
+
+    public void setUsingBrackets(boolean usingBrackets) {
+        this.usingBrackets = usingBrackets;
+    }
+
+    public ArrayList<Column> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(ArrayList<Column> columns) {
+        this.columns = Objects.requireNonNull(columns);
+    }
+
+    public ArrayList<Expression> getExpressions() {
+        return expressions;
+    }
+
+    public void setExpressions(ArrayList<Expression> expressions) {
+        this.expressions = Objects.requireNonNull(expressions);
+    }
+
+    public void add(Column column, Expression expression) {
+        columns.add(column);
+        expressions.add(expression);
+    };
+
+    public void add(Column column) {
+        columns.add(column);
+    };
+
+    public void add(ExpressionList expressionList) {
+        expressions.addAll(expressionList.getExpressions());
+    };
+}
