8d6f0ff5e9f01fbbaeca82fd42931b9fe24de20e
Andreas Reichel <andreas@manticore-projects.com>
2021-09-06 07:30:30 +0700

    Fixes #1325
    Removes redundant production Identifier() and uses RelObjectnameWithoutValue() instead for MS SQL Server Hints

--- src/test/java/net/sf/jsqlparser/statement/select/SelectTest.java
+++ src/test/java/net/sf/jsqlparser/statement/select/SelectTest.java
@@ -4776,4 +4776,11 @@ public class SelectTest {
                         "from dual \n" +
                         "group by (case when 1=1 then 'X' else 'Y' end), column1", true);
     }
+
+    @Test
+    public void testReservedKeywordsMSSQLUseIndexIssue1325() throws JSQLParserException {
+        // without extra brackets
+        assertSqlCanBeParsedAndDeparsed(
+                "SELECT col FROM table USE INDEX(primary)", true);
+    }
 }
