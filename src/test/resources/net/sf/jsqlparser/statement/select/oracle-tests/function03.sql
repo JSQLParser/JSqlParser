86f337dbafd10ab4f9d083bead2722699a203920
Andreas Reichel <andreas@manticore-projects.com>
2021-10-24 16:58:33 +0700

    Fix test resources

--- src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/function03.sql
+++ src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/function03.sql
@@ -13,5 +13,3 @@ from dual
 
 
 --@FAILURE: Encountered unexpected token: "(" "(" recorded first on Aug 3, 2021, 7:20:08 AM
---@FAILURE: Exception without message: net.sf.jsqlparser.JSQLParserException recorded first on 24 Oct 2021, 02:38:30
---@FAILURE: Encountered unexpected token: "trim" <K_STRING_FUNCTION_NAME> recorded first on 24 Oct 2021, 02:45:19
