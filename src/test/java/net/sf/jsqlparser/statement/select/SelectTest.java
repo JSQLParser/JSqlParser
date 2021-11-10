3186ba6cbe8131a9fd61d70fd12bbf3d8af973b6
EverSQL <tomer@eversql.com>
2021-11-10 20:06:51 +0200

    Add supported for quoted cast expressions for PostgreSQL

--- src/test/java/net/sf/jsqlparser/statement/select/SelectTest.java
+++ src/test/java/net/sf/jsqlparser/statement/select/SelectTest.java
@@ -1169,6 +1169,11 @@ public class SelectTest {
     }
 
     @Test
+    public void testQuotedCastExpression() throws JSQLParserException {
+        assertSqlCanBeParsedAndDeparsed("SELECT col FROM test WHERE status = CASE WHEN anothercol = 5 THEN 'pending'::\"enum_test\" END");
+    }
+
+    @Test
     public void testWhere() throws JSQLParserException {
 
         final String statement = "SELECT * FROM tab1 WHERE";
