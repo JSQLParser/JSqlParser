7279d8a91e992870ac1cc4fcb0f31ac7b5f52c44
Andreas Reichel <andreas@manticore-projects.com>
2022-07-18 12:32:54 +0700

    Appease Codacy

--- src/test/java/net/sf/jsqlparser/parser/CCJSqlParserUtilTest.java
+++ src/test/java/net/sf/jsqlparser/parser/CCJSqlParserUtilTest.java
@@ -29,7 +29,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
