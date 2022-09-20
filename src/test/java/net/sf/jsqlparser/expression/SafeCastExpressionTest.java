d9985ae4f559cda63acd86faae0e943844ac3684
dequn <dqzhang.chn@gmail.com>
2022-09-21 02:22:25 +0800

    Support BigQuery SAFE_CAST (#1622) (#1634)
    Co-authored-by: Zhang, Dequn <deqzhang@paypal.com>

---
+++ src/test/java/net/sf/jsqlparser/expression/SafeCastExpressionTest.java
@@ -0,0 +1,23 @@
+/*-
+ * #%L
+ * JSQLParser library
+ * %%
+ * Copyright (C) 2004 - 2022 JSQLParser
+ * %%
+ * Dual licensed under GNU LGPL 2.1 or Apache License 2.0
+ * #L%
+ */
+package net.sf.jsqlparser.expression;
+
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.test.TestUtils;
+import org.junit.jupiter.api.Test;
+
+public class SafeCastExpressionTest {
+
+    @Test
+    public void testSafeCast() throws JSQLParserException {
+        TestUtils.assertExpressionCanBeParsedAndDeparsed("SAFE_CAST(ROW(dataid, value, calcMark) AS ROW(datapointid CHAR, value CHAR, calcMark CHAR))", true);
+        TestUtils.assertExpressionCanBeParsedAndDeparsed("SAFE_CAST(ROW(dataid, value, calcMark) AS testcol)", true);
+    }
+}
