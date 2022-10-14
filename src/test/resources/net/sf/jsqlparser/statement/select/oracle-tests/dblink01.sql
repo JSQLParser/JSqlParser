e3019edb0f541f820789663f30d87ffe439ccd37
Andreas Reichel <andreas@manticore-projects.com>
2022-10-14 17:35:53 +0700

    fix: Revert correct test case

--- src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/dblink01.sql
+++ src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/dblink01.sql
@@ -14,4 +14,3 @@ select last_name, department_name
 
 
 --@SUCCESSFULLY_PARSED_AND_DEPARSED first on Aug 3, 2021, 7:20:08 AM
---@FAILURE: select last_name,department_name from employees.remote,departments where employees.department_id=departments.department_id recorded first on 14 Oct 2022, 17:17:32
