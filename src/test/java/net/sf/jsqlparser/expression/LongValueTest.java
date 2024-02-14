0bce4648221a242887eef3a213a650dac65dc381
pingpingy1 <pingpingy@kaist.ac.kr>
2024-02-14 16:09:47 +0900

    fix checkstyle issues

--- src/test/java/net/sf/jsqlparser/expression/LongValueTest.java
+++ src/test/java/net/sf/jsqlparser/expression/LongValueTest.java
@@ -40,7 +40,7 @@ public class LongValueTest {
             value.getValue();
             fail("should not work");
         } catch (Exception e) {
-            //expected to fail
+            // expected to fail
         }
         assertEquals(new BigInteger(largeNumber), value.getBigIntegerValue());
     }
