8461e8ad1a3f5e3d6661c4365e0925e8cb565194
Andreas Reichel <andreas@manticore-projects.com>
2024-04-13 11:52:10 +0700

    fix: correct the wrong Assertion
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

--- src/test/java/net/sf/jsqlparser/expression/BinaryExpressionTest.java
+++ src/test/java/net/sf/jsqlparser/expression/BinaryExpressionTest.java
@@ -1,13 +1,13 @@
 package net.sf.jsqlparser.expression;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.locationtech.jts.util.Assert;
 
 class BinaryExpressionTest {
 
     @Test
     void testAddition() {
         Expression addition = BinaryExpression.add(new LongValue(1), new LongValue(1));
-        Assert.equals("1 + 1", addition.toString());
+        Assertions.assertEquals("1 + 1", addition.toString());
     }
 }
