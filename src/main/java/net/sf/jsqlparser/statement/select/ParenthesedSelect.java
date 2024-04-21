f16999393589702f4c238659416e0f96c9f813b4
Andreas Reichel <andreas@manticore-projects.com>
2024-04-21 22:07:45 +0700

    feat: add `ParenthesedSelect` delegate
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

--- src/main/java/net/sf/jsqlparser/statement/select/ParenthesedSelect.java
+++ src/main/java/net/sf/jsqlparser/statement/select/ParenthesedSelect.java
@@ -14,6 +14,7 @@ import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.schema.Table;
 
 import java.util.Collection;
+import java.util.List;
 
 public class ParenthesedSelect extends Select implements FromItem {
     Alias alias;
@@ -132,6 +133,11 @@ public class ParenthesedSelect extends Select implements FromItem {
 
     public ParenthesedSelect withSelect(Select selectBody) {
         setSelect(selectBody);
+        return this;
+    }
+
+    public ParenthesedSelect withOrderByElements(List<OrderByElement> orderByElements) {
+        super.setOrderByElements(orderByElements);
         return this;
     }
 
