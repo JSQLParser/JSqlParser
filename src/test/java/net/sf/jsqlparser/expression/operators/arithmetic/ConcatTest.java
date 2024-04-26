c2ff4ae7dd3dc767385b51cad4505aac0e30bfe1
Andreas Reichel <andreas@manticore-projects.com>
2024-04-26 15:23:13 +0700

    build: tweak the Maven build
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

--- src/test/java/net/sf/jsqlparser/expression/operators/arithmetic/ConcatTest.java
+++ src/test/java/net/sf/jsqlparser/expression/operators/arithmetic/ConcatTest.java
@@ -1,3 +1,12 @@
+/*-
+ * #%L
+ * JSQLParser library
+ * %%
+ * Copyright (C) 2004 - 2024 JSQLParser
+ * %%
+ * Dual licensed under GNU LGPL 2.1 or Apache License 2.0
+ * #L%
+ */
 package net.sf.jsqlparser.expression.operators.arithmetic;
 
 import net.sf.jsqlparser.expression.Expression;
