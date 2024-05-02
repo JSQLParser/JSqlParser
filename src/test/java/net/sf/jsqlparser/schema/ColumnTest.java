82992fffb2d8450556da3ee81f9a53248b1562de
Chris Crabtree <chris.crabtree@matillion.com>
2024-05-02 16:13:42 +0100

    feat: Checkstyle

--- src/test/java/net/sf/jsqlparser/schema/ColumnTest.java
+++ src/test/java/net/sf/jsqlparser/schema/ColumnTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
