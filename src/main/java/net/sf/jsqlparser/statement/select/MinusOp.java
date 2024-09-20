9a62bc034838265692218af217cc47ce7e4dc738
sunanda-vs <22m2107@iitb.ac.in>
2024-09-20 16:59:51 +0530

    added support for minus all; except all; intersect all;

--- src/main/java/net/sf/jsqlparser/statement/select/MinusOp.java
+++ src/main/java/net/sf/jsqlparser/statement/select/MinusOp.java
@@ -12,8 +12,46 @@ package net.sf.jsqlparser.statement.select;
 import net.sf.jsqlparser.statement.select.SetOperationList.SetOperationType;
 
 public class MinusOp extends SetOperation {
-
+    private boolean distinct;
+    private boolean all;
+    
     public MinusOp() {
         super(SetOperationType.MINUS);
+    }
+    public boolean isAll() {
+        return all;
+    }
+
+    public void setAll(boolean all) {
+        this.all = all;
+    }
+
+    public boolean isDistinct() {
+        return distinct;
+    }
+
+    public void setDistinct(boolean distinct) {
+        this.distinct = distinct;
+    }
+
+    @Override
+    public String toString() {
+        String allDistinct = "";
+        if (isAll()) {
+            allDistinct = " ALL";
+        } else if (isDistinct()) {
+            allDistinct = " DISTINCT";
+        }
+        return super.toString() + allDistinct;
+    }
+
+    public MinusOp withDistinct(boolean distinct) {
+        this.setDistinct(distinct);
+        return this;
+    }
+
+    public MinusOp withAll(boolean all) {
+        this.setAll(all);
+        return this;
     }
 }
