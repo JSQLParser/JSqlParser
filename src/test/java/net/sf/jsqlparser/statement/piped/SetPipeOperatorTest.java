dd4068588b4bb22d4677dac4e597efdd23794302
Andreas Reichel <andreas@manticore-projects.com>
2025-03-10 16:16:51 +0700

    build: bring back OSGi for Snapshots with Gradle
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

--- src/test/java/net/sf/jsqlparser/statement/piped/SetPipeOperatorTest.java
+++ src/test/java/net/sf/jsqlparser/statement/piped/SetPipeOperatorTest.java
@@ -1,3 +1,12 @@
+/*-
+ * #%L
+ * JSQLParser library
+ * %%
+ * Copyright (C) 2004 - 2025 JSQLParser
+ * %%
+ * Dual licensed under GNU LGPL 2.1 or Apache License 2.0
+ * #L%
+ */
 package net.sf.jsqlparser.statement.piped;
 
 import net.sf.jsqlparser.JSQLParserException;
