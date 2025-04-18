98e03d4c5e5f6590e9c896c710792fe36f505367
manticore-projects <andreas@manticore-projects.com>
2025-04-18 15:04:52 +0700

    feat: link all Expression AST Nodes
    - fixes #1339
    
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

--- src/test/java/net/sf/jsqlparser/parser/ASTNodeAccessImplTest.java
+++ src/test/java/net/sf/jsqlparser/parser/ASTNodeAccessImplTest.java
@@ -11,9 +11,11 @@ package net.sf.jsqlparser.parser;
 
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.expression.AnalyticExpression;
+import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.select.SelectItem;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -32,5 +34,26 @@ class ASTNodeAccessImplTest {
 
         assertInstanceOf(SelectItem.class, expression.getParent());
         assertEquals(select, expression.getParent(Select.class));
+    }
+
+    @Test
+    void testGetWherePositionIssue1339() throws JSQLParserException {
+        // WHERE expression at line 4 column 7
+        String sqlStr = "select listagg(sellerid)\n"
+                + "within group (order by sellerid)\n"
+                + "over() AS list from winsales\n"
+                + "WHERE a=b\n"
+                + "ORDER BY 1;";
+        PlainSelect select = (PlainSelect) CCJSqlParserUtil.parse(sqlStr);
+        Expression whereExpression = select.getWhere();
+
+        final SimpleNode node = whereExpression.getASTNode();
+        if (node != null) {
+            Token token = node.jjtGetFirstToken();
+            Assertions.assertEquals(4, token.beginLine);
+            Assertions.assertEquals(7, token.beginColumn);
+        } else {
+            throw new RuntimeException("Node not found.");
+        }
     }
 }
