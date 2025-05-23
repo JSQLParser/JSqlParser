0f9e477944332c4711b4056984e7d383f15c9237
manticore-projects <andreas@manticore-projects.com>
2025-05-23 14:10:11 +0700

    fix: precedence of the `InExpression`
    - fixes #2244
    
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

--- src/test/java/net/sf/jsqlparser/expression/operators/relational/InExpressionTest.java
+++ src/test/java/net/sf/jsqlparser/expression/operators/relational/InExpressionTest.java
@@ -10,7 +10,10 @@
 package net.sf.jsqlparser.expression.operators.relational;
 
 import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
+import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.test.TestUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,6 +31,14 @@ class InExpressionTest {
     void testOracleInWithBrackets() throws JSQLParserException {
         String sqlStr = "select 1 from dual where a in (1) ";
         TestUtils.assertSqlCanBeParsedAndDeparsed(sqlStr, true);
+    }
+
+    @Test
+    void testPrecedenceIssue2244() throws JSQLParserException {
+        String sqlStr = "select * from `T_DEMO` where a in (1,3,2) or b = 2";
+        PlainSelect select = (PlainSelect) TestUtils.assertSqlCanBeParsedAndDeparsed(sqlStr, true);
+
+        Assertions.assertInstanceOf(OrExpression.class, select.getWhere());
     }
 
 }
