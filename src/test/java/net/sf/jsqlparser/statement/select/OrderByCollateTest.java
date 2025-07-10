574518747e6941eb908de3a5a997b13506d604a3
Sam Sovereign <5209310+ldaIas@users.noreply.github.com>
2025-07-10 17:25:10 -0600

    rm unnecessary test

--- src/test/java/net/sf/jsqlparser/statement/select/OrderByCollateTest.java
+++ src/test/java/net/sf/jsqlparser/statement/select/OrderByCollateTest.java
@@ -1,12 +1,8 @@
 package net.sf.jsqlparser.statement.select;
 
 import static net.sf.jsqlparser.test.TestUtils.assertSqlCanBeParsedAndDeparsed;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.parser.CCJSqlParserUtil;
-import net.sf.jsqlparser.expression.CollateExpression;
 import org.junit.jupiter.api.Test;
 
 public class OrderByCollateTest {
@@ -33,24 +29,5 @@ public class OrderByCollateTest {
     public void testOrderByWithCollateAndNulls() throws JSQLParserException {
         String sql = "SELECT * FROM a ORDER BY col COLLATE \"C\" DESC NULLS LAST";
         assertSqlCanBeParsedAndDeparsed(sql);
-    }
-
-    @Test
-    public void testOrderByCollateStructure() throws JSQLParserException {
-        String sql = "SELECT * FROM a ORDER BY col COLLATE \"C\" ASC";
-        Select select = (Select) CCJSqlParserUtil.parse(sql);
-        PlainSelect plainSelect = (PlainSelect) select;
-        
-        assertNotNull(plainSelect.getOrderByElements());
-        assertEquals(1, plainSelect.getOrderByElements().size());
-        
-        OrderByElement orderByElement = plainSelect.getOrderByElements().get(0);
-        assertNotNull(orderByElement.getExpression());
-        
-        // The expression should be a CollateExpression
-        if (orderByElement.getExpression() instanceof CollateExpression) {
-            CollateExpression collateExpr = (CollateExpression) orderByElement.getExpression();
-            assertEquals("\"C\"", collateExpr.getCollate());
-        }
     }
 }
