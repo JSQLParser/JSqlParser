4cf31e306bfce82f247c7015d6ab59ce08d57f1f
Andreas Neumann <ANeumann0815@googlemail.com>
2025-10-10 12:21:12 +0200

    [feat] Support for FOR READ ONLY/FOR FETCH ONLY

--- src/test/java/net/sf/jsqlparser/statement/select/DB2Test.java
+++ src/test/java/net/sf/jsqlparser/statement/select/DB2Test.java
@@ -12,12 +12,26 @@ package net.sf.jsqlparser.statement.select;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.test.TestUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DB2Test {
     @Test
     void testDB2SpecialRegister() throws JSQLParserException {
         String sqlStr =
                 "SELECT * FROM TABLE1 where COL_WITH_TIMESTAMP <= CURRENT TIMESTAMP - CURRENT TIMEZONE";
+        TestUtils.assertSqlCanBeParsedAndDeparsed(sqlStr, true);
+    }
+
+    @ParameterizedTest
+    @ValueSource( strings = {
+            "SELECT * FROM table WITH UR",
+            "SELECT * FROM table WITH UR FOR READ ONLY",
+            "SELECT * FROM table FOR READ ONLY",
+            "SELECT * FROM table FOR FETCH ONLY",
+            "SELECT * FROM table FETCH FIRST 100 ROWS ONLY FOR READ ONLY"
+    })
+    void testWithIsolationLevelAndReadOnlyModes(String sqlStr) throws JSQLParserException {
         TestUtils.assertSqlCanBeParsedAndDeparsed(sqlStr, true);
     }
 }
