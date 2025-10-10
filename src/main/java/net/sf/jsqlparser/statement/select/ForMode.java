e400444402c7a6eff694ac2dadd852a6632cf546
Andreas Neumann <aneumann0815@googlemail.com>
2025-10-10 13:01:28 +0200

    [feat] Support for FOR READ ONLY/FOR FETCH ONLY (#2325)
    * [feat] Support for FOR READ ONLY/FOR FETCH ONLY
    
    * [chore] Spotless

--- src/main/java/net/sf/jsqlparser/statement/select/ForMode.java
+++ src/main/java/net/sf/jsqlparser/statement/select/ForMode.java
@@ -20,7 +20,10 @@ public enum ForMode {
 
     NO_KEY_UPDATE("NO KEY UPDATE"),
 
-    KEY_SHARE("KEY SHARE");
+    KEY_SHARE("KEY SHARE"),
+
+    // https://www.ibm.com/docs/en/db2-for-zos/13.0.0?topic=statement-read-only-clause
+    READ_ONLY("READ ONLY"), FETCH_ONLY("FETCH ONLY");
 
     private final String value;
 
