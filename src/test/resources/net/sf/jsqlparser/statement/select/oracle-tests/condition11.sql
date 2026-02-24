1f840d812db716c0c7866bb023be3f1f514b4c14
manticore-projects <andreas@manticore-projects.com>
2026-02-25 00:29:14 +0700

    test: register one more successful Oracle test
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>
    Signed-off-by: manticore-projects <andreas@manticore-projects.com>

--- src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/condition11.sql
+++ src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/condition11.sql
@@ -18,3 +18,4 @@ and 0 = Lib.SKU(X.sid, nvl(Z.cid, '^'))
 --@FAILURE: Encountered unexpected token: "(" "(" recorded first on Aug 3, 2021, 7:20:08 AM
 --@FAILURE: Encountered: "(" / "(", at line 14, column 26, in lexical state DEFAULT. recorded first on 15 May 2025, 16:24:08
 --@FAILURE: Encountered: <OPENING_BRACKET> / "(", at line 14, column 26, in lexical state DEFAULT. recorded first on 9 Jul 2025, 17:09:17
+--@SUCCESSFULLY_PARSED_AND_DEPARSED first on 25 Feb 2026, 00:22:20
