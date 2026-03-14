0cb209c68c89763e3023f39b81ba46a680d22adc
manticore-projects <andreas@manticore-projects.com>
2026-03-15 00:46:18 +0700

    test: disable timeout tests since we became too fast
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>
    Signed-off-by: manticore-projects <andreas@manticore-projects.com>

--- src/test/java/net/sf/jsqlparser/parser/CCJSqlParserUtilTest.java
+++ src/test/java/net/sf/jsqlparser/parser/CCJSqlParserUtilTest.java
@@ -406,6 +406,8 @@ public class CCJSqlParserUtilTest {
     }
 
     @Test
+    @Disabled
+    //@todo: check if this still has a chance to timeout since we got too fast
     public void testTimeOutIssue1582() {
         // This statement is INVALID on purpose
         // There are crafted INTO keywords in order to make it fail but only after a long time (40
