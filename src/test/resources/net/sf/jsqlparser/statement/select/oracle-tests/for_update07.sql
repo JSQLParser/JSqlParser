46db8d3b75b01266fbe7fd756be4c805763e47b3
Andreas Reichel <andreas@manticore-projects.com>
2026-07-30 08:28:52 +0700

    test: update test
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

--- src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/for_update07.sql
+++ src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/for_update07.sql
@@ -10,12 +10,4 @@
 select employee_id from (select employee_id+1 as employee_id from employees)
    for update of a, b.c, d skip locked
 
-
---@FAILURE: Encountered unexpected token: "," "," recorded first on Aug 3, 2021, 7:20:08 AM
---@FAILURE: Encountered: <K_COMMA> / ",", at line 11, column 19, in lexical state DEFAULT. recorded first on 15 May 2025, 16:24:08<<<<<<< ours
---@FAILURE: --- recorded first on 11 Apr 2026, 22:18:19
-||||||| original
-
-=======
---@SUCCESSFULLY_PARSED_AND_DEPARSED first on Apr 11, 2026, 4:05:21 PM
->>>>>>> theirs
+--@SUCCESSFULLY_PARSED_AND_DEPARSED first on Jul 30, 2026, 8:19:20 AM
