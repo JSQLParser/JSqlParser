JSQLParser/JSqlParser

commit 5fe938bc369a83bb9ab1c82aa791f74d01b55a71

feat: `CORRESPONDING` modifier for `SetOperator`
Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

src/main/java/net/sf/jsqlparser/statement/select/SetOperation.java
@@ -13,6 +13,23 @@ import net.sf.jsqlparser.parser.ASTNodeAccessImpl;
 import net.sf.jsqlparser.statement.select.SetOperationList.SetOperationType;
 
 public abstract class SetOperation extends ASTNodeAccessImpl {
+    String modifier = "";
+
+    public boolean isAll() {
+        return modifier.contains("ALL");
+    }
+
+    public void setAll(boolean all) {
+        this.modifier = "ALL";
+    }
+
+    public boolean isDistinct() {
+        return modifier.contains("DISTINCT");
+    }
+
+    public void setDistinct(boolean distinct) {
+        this.modifier = "DISTINCT";
+    }
 
     private SetOperationType type;
 
@@ -22,6 +39,6 @@ public abstract class SetOperation extends ASTNodeAccessImpl {
 
     @Override
     public String toString() {
-        return type.name();
+        return modifier == null || modifier.isEmpty() ? type.name() : type.name() + " " + modifier;
     }
 }

src/main/java/net/sf/jsqlparser/statement/select/ExceptOp.java
@@ -13,38 +13,13 @@ import net.sf.jsqlparser.statement.select.SetOperationList.SetOperationType;
 
 public class ExceptOp extends SetOperation {
 
-    private boolean distinct;
-    private boolean all;
-
     public ExceptOp() {
+        this("");
+    }
+
+    public ExceptOp(String modifier) {
         super(SetOperationType.EXCEPT);
-    }
-
-    public boolean isAll() {
-        return all;
-    }
-
-    public void setAll(boolean all) {
-        this.all = all;
-    }
-
-    public boolean isDistinct() {
-        return distinct;
-    }
-
-    public void setDistinct(boolean distinct) {
-        this.distinct = distinct;
-    }
-
-    @Override
-    public String toString() {
-        String allDistinct = "";
-        if (isAll()) {
-            allDistinct = " ALL";
-        } else if (isDistinct()) {
-            allDistinct = " DISTINCT";
-        }
-        return super.toString() + allDistinct;
+        this.modifier = modifier;
     }
 
     public ExceptOp withDistinct(boolean distinct) {
@@ -54,6 +29,11 @@ public class ExceptOp extends SetOperation {
 
     public ExceptOp withAll(boolean all) {
         this.setAll(all);
+        return this;
+    }
+
+    public ExceptOp withModifier(String modifier) {
+        this.modifier = modifier;
         return this;
     }
 }

src/main/java/net/sf/jsqlparser/statement/select/IntersectOp.java
@@ -13,37 +13,13 @@ import net.sf.jsqlparser.statement.select.SetOperationList.SetOperationType;
 
 public class IntersectOp extends SetOperation {
 
-    private boolean distinct;
-    private boolean all;
-
     public IntersectOp() {
+        this("");
+    }
+
+    public IntersectOp(String modifier) {
         super(SetOperationType.INTERSECT);
-    }
-    public boolean isAll() {
-        return all;
-    }
-
-    public void setAll(boolean all) {
-        this.all = all;
-    }
-
-    public boolean isDistinct() {
-        return distinct;
-    }
-
-    public void setDistinct(boolean distinct) {
-        this.distinct = distinct;
-    }
-
-    @Override
-    public String toString() {
-        String allDistinct = "";
-        if (isAll()) {
-            allDistinct = " ALL";
-        } else if (isDistinct()) {
-            allDistinct = " DISTINCT";
-        }
-        return super.toString() + allDistinct;
+        this.modifier = modifier;
     }
 
     public IntersectOp withDistinct(boolean distinct) {
@@ -53,6 +29,11 @@ public class IntersectOp extends SetOperation {
 
     public IntersectOp withAll(boolean all) {
         this.setAll(all);
+        return this;
+    }
+
+    public IntersectOp withModifier(String modifier) {
+        this.modifier = modifier;
         return this;
     }
 }

src/main/java/net/sf/jsqlparser/statement/select/MinusOp.java
@@ -12,37 +12,13 @@ package net.sf.jsqlparser.statement.select;
 import net.sf.jsqlparser.statement.select.SetOperationList.SetOperationType;
 
 public class MinusOp extends SetOperation {
-    private boolean distinct;
-    private boolean all;
-    
     public MinusOp() {
+        this("");
+    }
+
+    public MinusOp(String modifier) {
         super(SetOperationType.MINUS);
-    }
-    public boolean isAll() {
-        return all;
-    }
-
-    public void setAll(boolean all) {
-        this.all = all;
-    }
-
-    public boolean isDistinct() {
-        return distinct;
-    }
-
-    public void setDistinct(boolean distinct) {
-        this.distinct = distinct;
-    }
-
-    @Override
-    public String toString() {
-        String allDistinct = "";
-        if (isAll()) {
-            allDistinct = " ALL";
-        } else if (isDistinct()) {
-            allDistinct = " DISTINCT";
-        }
-        return super.toString() + allDistinct;
+        this.modifier = modifier;
     }
 
     public MinusOp withDistinct(boolean distinct) {
@@ -52,6 +28,11 @@ public class MinusOp extends SetOperation {
 
     public MinusOp withAll(boolean all) {
         this.setAll(all);
+        return this;
+    }
+
+    public MinusOp withModifier(String modifier) {
+        this.modifier = modifier;
         return this;
     }
 }

src/main/java/net/sf/jsqlparser/statement/select/UnionOp.java
@@ -12,39 +12,13 @@ package net.sf.jsqlparser.statement.select;
 import net.sf.jsqlparser.statement.select.SetOperationList.SetOperationType;
 
 public class UnionOp extends SetOperation {
-
-    private boolean distinct;
-    private boolean all;
-
     public UnionOp() {
+        this("");
+    }
+
+    public UnionOp(String modifier) {
         super(SetOperationType.UNION);
-    }
-
-    public boolean isAll() {
-        return all;
-    }
-
-    public void setAll(boolean all) {
-        this.all = all;
-    }
-
-    public boolean isDistinct() {
-        return distinct;
-    }
-
-    public void setDistinct(boolean distinct) {
-        this.distinct = distinct;
-    }
-
-    @Override
-    public String toString() {
-        String allDistinct = "";
-        if (isAll()) {
-            allDistinct = " ALL";
-        } else if (isDistinct()) {
-            allDistinct = " DISTINCT";
-        }
-        return super.toString() + allDistinct;
+        this.modifier = modifier;
     }
 
     public UnionOp withDistinct(boolean distinct) {
@@ -54,6 +28,11 @@ public class UnionOp extends SetOperation {
 
     public UnionOp withAll(boolean all) {
         this.setAll(all);
+        return this;
+    }
+
+    public UnionOp withModifier(String modifier) {
+        this.modifier = modifier;
         return this;
     }
 }

src/main/jjtree/net/sf/jsqlparser/parser/JSqlParserCC.jjt
@@ -273,6 +273,7 @@ TOKEN: /* SQL Keywords. prefixed with K_ to avoid name clashes */
 |   <K_BLOCK: "BLOCK">
 |   <K_BOOLEAN:"BOOLEAN">
 |   <K_BOTH:"BOTH">
+|   <K_BRANCH:"BRANCH">
 |   <K_BROWSE:"BROWSE">
 |   <K_BY:"BY">
 |   <K_BYTES: "BYTES">
@@ -2094,7 +2095,7 @@ SessionStatement SessionStatement():
     Token idToken = null;
 }
 {
-    <K_SESSION>
+    ( <K_SESSION> | <K_BRANCH> )
     (
         actionToken = <K_START>
         |
@@ -3174,7 +3175,7 @@ String RelObjectNameWithoutValue() :
 {    Token tk = null; }
 {
     ( tk=<DATA_TYPE> | tk=<S_IDENTIFIER> | tk=<S_QUOTED_IDENTIFIER> |  tk=<K_DATE_LITERAL> | tk=<K_DATETIMELITERAL> | tk=<K_STRING_FUNCTION_NAME> | tk=<K_ISOLATION> | tk=<K_TIME_KEY_EXPR> | tk=<K_TEXT_LITERAL> 
-       | tk="ACTION" | tk="ACTIVE" | tk="ADD" | tk="ADVANCE" | tk="ADVISE" | tk="AGAINST" | tk="AGGREGATE" | tk="ALGORITHM" | tk="ALIGN" | tk="ALTER" | tk="ALWAYS" | tk="ANALYZE" | tk="APPEND_ONLY" | tk="APPLY" | tk="APPROXIMATE" | tk="ARCHIVE" | tk="ARRAY" | tk="ASC" | tk="ASYMMETRIC" | tk="AT" | tk="AUTHORIZATION" | tk="AUTO" | tk="AUTO_INCREMENT" | tk="AZURE" | tk="BASE64" | tk="BEFORE" | tk="BEGIN" | tk="BERNOULLI" | tk="BINARY" | tk="BIT" | tk="BLOBSTORAGE" | tk="BLOCK" | tk="BOOLEAN" | tk="BROWSE" | tk="BUFFERS" | tk="BY" | tk="BYTE" | tk="BYTES" | tk="CACHE" | tk="CALL" | tk="CASCADE" | tk="CASE" | tk="CAST" | tk="CERTIFICATE" | tk="CHANGE" | tk="CHANGES" | tk="CHAR" | tk="CHARACTER" | tk="CHECKPOINT" | tk="CLOSE" | tk="CLOUD" | tk="COALESCE" | tk="COLLATE" | tk="COLUMN" | tk="COLUMNS" | tk="COMMENT" | tk="COMMENTS" | tk="COMMIT" | tk="CONCURRENTLY" | tk="CONFLICT" | tk="CONSTRAINTS" | tk="CONVERT" | tk="CORRESPONDING" | tk="COSTS" | tk="COUNT" | tk="CREATED" | tk="CS" | tk="CYCLE" | tk="DATA" | tk="DATABASE" | tk="DATETIME" | tk="DBA_RECYCLEBIN" | tk="DDL" | tk="DECLARE" | tk="DEFAULTS" | tk="DEFERRABLE" | tk="DELAYED" | tk="DELETE" | tk="DELIMIT" | tk="DELIMITER" | tk="DESC" | tk="DESCRIBE" | tk="DISABLE" | tk="DISCARD" | tk="DISCONNECT" | tk="DIV" | tk="DML" | tk="DO" | tk="DOMAIN" | tk="DRIVER" | tk="DROP" | tk="DUMP" | tk="DUPLICATE" | tk="ELEMENTS" | tk="EMIT" | tk="ENABLE" | tk="ENCODING" | tk="ENCRYPTION" | tk="END" | tk="ENFORCED" | tk="ENGINE" | tk="ERROR" | tk="ESCAPE" | tk="EXA" | tk="EXCHANGE" | tk="EXCLUDE" | tk="EXCLUDING" | tk="EXEC" | tk="EXECUTE" | tk="EXPLAIN" | tk="EXPLICIT" | tk="EXPORT" | tk="EXTENDED" | tk="EXTRACT" | tk="FILTER" | tk="FIRST" | tk="FLUSH" | tk="FN" | tk="FOLLOWING" | tk="FORMAT" | tk="FULLTEXT" | tk="FUNCTION" | tk="GRANT" | tk="GROUP_CONCAT" | tk="GUARD" | tk="HASH" | tk="HIGH" | tk="HIGH_PRIORITY" | tk="HISTORY" | tk="HOPPING" | tk="IDENTIFIED" | tk="IDENTITY" | tk="INCLUDE" | tk="INCLUDE_NULL_VALUES" | tk="INCLUDING" | tk="INCREMENT" | tk="INDEX" | tk="INFORMATION" | tk="INSERT" | tk="INTERLEAVE" | tk="INTERPRET" | tk="INVALIDATE" | tk="INVERSE" | tk="INVISIBLE" | tk="ISNULL" | tk="JDBC" | tk="JSON" | tk="JSON_ARRAY" | tk="JSON_ARRAYAGG" | tk="JSON_OBJECT" | tk="JSON_OBJECTAGG" | tk="KEEP" | tk="KEY" | tk="KEYS" | tk="KEY_BLOCK_SIZE" | tk="KILL" | tk="LAST" | tk="LEADING" | tk="LESS" | tk="LINK" | tk="LOCAL" | tk="LOCK" | tk="LOCKED" | tk="LOG" | tk="LONGTEXT" | tk="LOOP" | tk="LOW" | tk="LOW_PRIORITY" | tk="LTRIM" | tk="MATCH" | tk="MATCHED" | tk="MATCH_ALL" | tk="MATCH_ANY" | tk="MATCH_PHRASE" | tk="MATCH_PHRASE_PREFIX" | tk="MATCH_REGEXP" | tk="MATERIALIZED" | tk="MAX" | tk="MAXVALUE" | tk="MEDIUMTEXT" | tk="MEMBER" | tk="MERGE" | tk="MIN" | tk="MINVALUE" | tk="MODIFY" | tk="MOVEMENT" | tk="NAME" | tk="NAMES" | tk="NEVER" | tk="NEXT" | tk="NO" | tk="NOCACHE" | tk="NOKEEP" | tk="NOLOCK" | tk="NOMAXVALUE" | tk="NOMINVALUE" | tk="NONE" | tk="NOORDER" | tk="NOTHING" | tk="NOTNULL" | tk="NOVALIDATE" | tk="NOWAIT" | tk="NULLS" | tk="OF" | tk="OFF" | tk="OPEN" | tk="ORA" | tk="ORDINALITY" | tk="OVER" | tk="OVERFLOW" | tk="OVERLAPS" | tk="OVERRIDING" | tk="OVERWRITE" | tk="PADDING" | tk="PARALLEL" | tk="PARENT" | tk="PARSER" | tk="PARTITION" | tk="PARTITIONING" | tk="PATH" | tk="PERCENT" | tk="PLACING" | tk="PLAN" | tk="PLUS" | tk="PRECEDING" | tk="PRIMARY" | tk="PURGE" | tk="QUERY" | tk="QUICK" | tk="QUIESCE" | tk="RANGE" | tk="RAW" | tk="READ" | tk="REBUILD" | tk="RECURSIVE" | tk="RECYCLEBIN" | tk="REFERENCES" | tk="REFRESH" | tk="REGEXP" | tk="REGEXP_LIKE" | tk="REGISTER" | tk="REJECT" | tk="REMOTE" | tk="REMOVE" | tk="RENAME" | tk="REORGANIZE" | tk="REPAIR" | tk="REPEATABLE" | tk="REPLACE" | tk="RESET" | tk="RESPECT" | tk="RESTART" | tk="RESTRICT" | tk="RESTRICTED" | tk="RESUMABLE" | tk="RESUME" | tk="RETURN" | tk="RLIKE" | tk="ROLLBACK" | tk="ROLLUP" | tk="ROOT" | tk="ROW" | tk="ROWS" | tk="RR" | tk="RS" | tk="RTRIM" | tk="SAFE_CAST" | tk="SAVEPOINT" | tk="SCHEMA" | tk="SECURE" | tk="SEED" | tk="SEPARATOR" | tk="SEQUENCE" | tk="SESSION" | tk="SETS" | tk="SHARE" | tk="SHOW" | tk="SHUTDOWN" | tk="SIBLINGS" | tk="SIGNED" | tk="SIMILAR" | tk="SIZE" | tk="SKIP" | tk="SPATIAL" | tk="STORED" | tk="STREAM" | tk="STRICT" | tk="STRING" | tk="STRUCT" | tk="SUMMARIZE" | tk="SUSPEND" | tk="SWITCH" | tk="SYMMETRIC" | tk="SYNONYM" | tk="SYSTEM" | tk="SYSTEM_TIME" | tk="SYSTEM_TIMESTAMP" | tk="SYSTEM_VERSION" | tk="TABLE" | tk="TABLESPACE" | tk="TEMP" | tk="TEMPORARY" | tk="TEXT" | tk="THAN" | tk="THEN" | tk="TIMEOUT" | tk="TIMESTAMPTZ" | tk="TIMEZONE" | tk="TINYTEXT" | tk="TO" | tk="TRIGGER" | tk="TRUNCATE" | tk="TRY_CAST" | tk="TUMBLING" | tk="TYPE" | tk="UNLIMITED" | tk="UNLOGGED" | tk="UNQIESCE" | tk="UNSIGNED" | tk="UPDATE" | tk="UPSERT" | tk="UR" | tk="USER" | tk="VALIDATE" | tk="VALIDATION" | tk="VERBOSE" | tk="VERSION" | tk="VIEW" | tk="VISIBLE" | tk="VOLATILE" | tk="WAIT" | tk="WITHIN" | tk="WITHOUT" | tk="WITHOUT_ARRAY_WRAPPER" | tk="WORK" | tk="XML" | tk="XMLAGG" | tk="XMLDATA" | tk="XMLSCHEMA" | tk="XMLTEXT" | tk="XSINIL" | tk="YAML" | tk="YES" | tk="ZONE" )
+       | tk="ACTION" | tk="ACTIVE" | tk="ADD" | tk="ADVANCE" | tk="ADVISE" | tk="AGAINST" | tk="AGGREGATE" | tk="ALGORITHM" | tk="ALIGN" | tk="ALTER" | tk="ALWAYS" | tk="ANALYZE" | tk="APPEND_ONLY" | tk="APPLY" | tk="APPROXIMATE" | tk="ARCHIVE" | tk="ARRAY" | tk="ASC" | tk="ASYMMETRIC" | tk="AT" | tk="AUTHORIZATION" | tk="AUTO" | tk="AUTO_INCREMENT" | tk="AZURE" | tk="BASE64" | tk="BEFORE" | tk="BEGIN" | tk="BERNOULLI" | tk="BINARY" | tk="BIT" | tk="BLOBSTORAGE" | tk="BLOCK" | tk="BOOLEAN" | tk="BRANCH" | tk="BROWSE" | tk="BUFFERS" | tk="BY" | tk="BYTE" | tk="BYTES" | tk="CACHE" | tk="CALL" | tk="CASCADE" | tk="CASE" | tk="CAST" | tk="CERTIFICATE" | tk="CHANGE" | tk="CHANGES" | tk="CHAR" | tk="CHARACTER" | tk="CHECKPOINT" | tk="CLOSE" | tk="CLOUD" | tk="COALESCE" | tk="COLLATE" | tk="COLUMN" | tk="COLUMNS" | tk="COMMENT" | tk="COMMENTS" | tk="COMMIT" | tk="CONCURRENTLY" | tk="CONFLICT" | tk="CONSTRAINTS" | tk="CONVERT" | tk="CORRESPONDING" | tk="COSTS" | tk="COUNT" | tk="CREATED" | tk="CS" | tk="CYCLE" | tk="DATA" | tk="DATABASE" | tk="DATETIME" | tk="DBA_RECYCLEBIN" | tk="DDL" | tk="DECLARE" | tk="DEFAULTS" | tk="DEFERRABLE" | tk="DELAYED" | tk="DELETE" | tk="DELIMIT" | tk="DELIMITER" | tk="DESC" | tk="DESCRIBE" | tk="DISABLE" | tk="DISCARD" | tk="DISCONNECT" | tk="DIV" | tk="DML" | tk="DO" | tk="DOMAIN" | tk="DRIVER" | tk="DROP" | tk="DUMP" | tk="DUPLICATE" | tk="ELEMENTS" | tk="EMIT" | tk="ENABLE" | tk="ENCODING" | tk="ENCRYPTION" | tk="END" | tk="ENFORCED" | tk="ENGINE" | tk="ERROR" | tk="ESCAPE" | tk="EXA" | tk="EXCHANGE" | tk="EXCLUDE" | tk="EXCLUDING" | tk="EXEC" | tk="EXECUTE" | tk="EXPLAIN" | tk="EXPLICIT" | tk="EXPORT" | tk="EXTENDED" | tk="EXTRACT" | tk="FILTER" | tk="FIRST" | tk="FLUSH" | tk="FN" | tk="FOLLOWING" | tk="FORMAT" | tk="FULLTEXT" | tk="FUNCTION" | tk="GRANT" | tk="GROUP_CONCAT" | tk="GUARD" | tk="HASH" | tk="HIGH" | tk="HIGH_PRIORITY" | tk="HISTORY" | tk="HOPPING" | tk="IDENTIFIED" | tk="IDENTITY" | tk="INCLUDE" | tk="INCLUDE_NULL_VALUES" | tk="INCLUDING" | tk="INCREMENT" | tk="INDEX" | tk="INFORMATION" | tk="INSERT" | tk="INTERLEAVE" | tk="INTERPRET" | tk="INVALIDATE" | tk="INVERSE" | tk="INVISIBLE" | tk="ISNULL" | tk="JDBC" | tk="JSON" | tk="JSON_ARRAY" | tk="JSON_ARRAYAGG" | tk="JSON_OBJECT" | tk="JSON_OBJECTAGG" | tk="KEEP" | tk="KEY" | tk="KEYS" | tk="KEY_BLOCK_SIZE" | tk="KILL" | tk="LAST" | tk="LEADING" | tk="LESS" | tk="LINK" | tk="LOCAL" | tk="LOCK" | tk="LOCKED" | tk="LOG" | tk="LONGTEXT" | tk="LOOP" | tk="LOW" | tk="LOW_PRIORITY" | tk="LTRIM" | tk="MATCH" | tk="MATCHED" | tk="MATCH_ALL" | tk="MATCH_ANY" | tk="MATCH_PHRASE" | tk="MATCH_PHRASE_PREFIX" | tk="MATCH_REGEXP" | tk="MATERIALIZED" | tk="MAX" | tk="MAXVALUE" | tk="MEDIUMTEXT" | tk="MEMBER" | tk="MERGE" | tk="MIN" | tk="MINVALUE" | tk="MODIFY" | tk="MOVEMENT" | tk="NAME" | tk="NAMES" | tk="NEVER" | tk="NEXT" | tk="NO" | tk="NOCACHE" | tk="NOKEEP" | tk="NOLOCK" | tk="NOMAXVALUE" | tk="NOMINVALUE" | tk="NONE" | tk="NOORDER" | tk="NOTHING" | tk="NOTNULL" | tk="NOVALIDATE" | tk="NOWAIT" | tk="NULLS" | tk="OF" | tk="OFF" | tk="OPEN" | tk="ORA" | tk="ORDINALITY" | tk="OVER" | tk="OVERFLOW" | tk="OVERLAPS" | tk="OVERRIDING" | tk="OVERWRITE" | tk="PADDING" | tk="PARALLEL" | tk="PARENT" | tk="PARSER" | tk="PARTITION" | tk="PARTITIONING" | tk="PATH" | tk="PERCENT" | tk="PLACING" | tk="PLAN" | tk="PLUS" | tk="PRECEDING" | tk="PRIMARY" | tk="PURGE" | tk="QUERY" | tk="QUICK" | tk="QUIESCE" | tk="RANGE" | tk="RAW" | tk="READ" | tk="REBUILD" | tk="RECURSIVE" | tk="RECYCLEBIN" | tk="REFERENCES" | tk="REFRESH" | tk="REGEXP" | tk="REGEXP_LIKE" | tk="REGISTER" | tk="REJECT" | tk="REMOTE" | tk="REMOVE" | tk="RENAME" | tk="REORGANIZE" | tk="REPAIR" | tk="REPEATABLE" | tk="REPLACE" | tk="RESET" | tk="RESPECT" | tk="RESTART" | tk="RESTRICT" | tk="RESTRICTED" | tk="RESUMABLE" | tk="RESUME" | tk="RETURN" | tk="RLIKE" | tk="ROLLBACK" | tk="ROLLUP" | tk="ROOT" | tk="ROW" | tk="ROWS" | tk="RR" | tk="RS" | tk="RTRIM" | tk="SAFE_CAST" | tk="SAVEPOINT" | tk="SCHEMA" | tk="SECURE" | tk="SEED" | tk="SEPARATOR" | tk="SEQUENCE" | tk="SESSION" | tk="SETS" | tk="SHARE" | tk="SHOW" | tk="SHUTDOWN" | tk="SIBLINGS" | tk="SIGNED" | tk="SIMILAR" | tk="SIZE" | tk="SKIP" | tk="SPATIAL" | tk="STORED" | tk="STREAM" | tk="STRICT" | tk="STRING" | tk="STRUCT" | tk="SUMMARIZE" | tk="SUSPEND" | tk="SWITCH" | tk="SYMMETRIC" | tk="SYNONYM" | tk="SYSTEM" | tk="SYSTEM_TIME" | tk="SYSTEM_TIMESTAMP" | tk="SYSTEM_VERSION" | tk="TABLE" | tk="TABLESPACE" | tk="TEMP" | tk="TEMPORARY" | tk="TEXT" | tk="THAN" | tk="THEN" | tk="TIMEOUT" | tk="TIMESTAMPTZ" | tk="TIMEZONE" | tk="TINYTEXT" | tk="TO" | tk="TRIGGER" | tk="TRUNCATE" | tk="TRY_CAST" | tk="TUMBLING" | tk="TYPE" | tk="UNLIMITED" | tk="UNLOGGED" | tk="UNQIESCE" | tk="UNSIGNED" | tk="UPDATE" | tk="UPSERT" | tk="UR" | tk="USER" | tk="VALIDATE" | tk="VALIDATION" | tk="VERBOSE" | tk="VERSION" | tk="VIEW" | tk="VISIBLE" | tk="VOLATILE" | tk="WAIT" | tk="WITHIN" | tk="WITHOUT" | tk="WITHOUT_ARRAY_WRAPPER" | tk="WORK" | tk="XML" | tk="XMLAGG" | tk="XMLDATA" | tk="XMLSCHEMA" | tk="XMLTEXT" | tk="XSINIL" | tk="YAML" | tk="YES" | tk="ZONE" )
     { return tk.image; }
 }
 
@@ -3662,15 +3663,42 @@ LimitPipeOperator LimitPipeOperator():
     }
 }
 
+// see https://manticore-projects.com/SQL2016Parser/syntax_snapshot.html#corresponding-spec
 String SetOperationModifier():
 {
-    Token token = null;
-    String modifier = null;
+    Token tk;
+    String modifier = "";
+    String identifier;
 }
 {
-    ( token=<K_ALL> | token=<K_DISTINCT> ) { modifier = token.image; }
-    [ (<K_BY> <K_NAME> { modifier+= " BY NAME"; } ) | (<K_STRICT> <K_CORRESPONDING> { modifier+= " STRICT CORRESPONDING"; }) ]
-
+    (
+        LOOKAHEAD(2) (
+             [ ( tk=<K_ALL> | tk="DISTINCT")  { modifier+=tk.image; } ]
+            <K_BY> <K_NAME> { modifier+= " BY NAME"; }
+            [
+                "MATCHING" { modifier+= " MATCHING"; }
+                "("
+                identifier = RelObjectNameExt() { modifier+="(" + identifier; }
+                ("," identifier = RelObjectNameExt()  { modifier+=", " + identifier; })*
+                ")" { modifier+=")"; }
+            ]
+        )
+        |
+        (
+            [ <K_STRICT>  { modifier+= " STRICT"; } ]
+            <K_CORRESPONDING> { modifier+= " CORRESPONDING"; }
+            [ (tk=<K_ALL> | tk="DISTINCT")  { modifier+=tk.image; } ]
+            [
+                <K_BY> { modifier+= " BY"; }[ (tk=<K_ALL> | tk="DISTINCT")  { modifier+=tk.image; } ]
+                "("
+                identifier = RelObjectNameExt() { modifier+="(" + identifier; }
+                ("," identifier = RelObjectNameExt()  { modifier+=", " + identifier;})*
+                ")" { modifier+=")"; }
+            ]
+        )
+        |
+        ( tk=<K_ALL> | tk=<K_DISTINCT> ) { modifier+=tk.image; }
+    )
     {
         return modifier;
     }
@@ -4159,6 +4187,7 @@ Select SetOperationList(Select select) #SetOperationList: {
     WithIsolation withIsolation = null;
     List<Select> selects = new ArrayList<Select>();
     List<SetOperation> operations = new ArrayList<SetOperation>();
+    String modifier = null;
 }
 {
 
@@ -4168,24 +4197,20 @@ Select SetOperationList(Select select) #SetOperationList: {
 
     ( LOOKAHEAD(2) (
             (
-                <K_UNION> { UnionOp union = new UnionOp(); linkAST(union,jjtThis); operations.add(union); }
-                [ <K_ALL> { union.setAll(true); } | <K_DISTINCT> { union.setDistinct(true); } [ (<K_BY> <K_NAME>) | (<K_STRICT> <K_CORRESPONDING>) ] ]
+                <K_UNION> [ modifier=SetOperationModifier() ] { UnionOp union = new UnionOp(modifier); linkAST(union,jjtThis); operations.add(union); }
 
             )
             |
             (
-                <K_INTERSECT> { IntersectOp intersect = new IntersectOp(); linkAST(intersect,jjtThis); operations.add(intersect); }
-                [ <K_ALL> { intersect.setAll(true); } | <K_DISTINCT> { intersect.setDistinct(true); } [ (<K_BY> <K_NAME>) | (<K_STRICT> <K_CORRESPONDING>) ] ]
+                <K_INTERSECT> [ modifier=SetOperationModifier() ] { IntersectOp intersect = new IntersectOp(modifier); linkAST(intersect,jjtThis); operations.add(intersect); }
             )
             |
             ( 
-                <K_MINUS> { MinusOp minus = new MinusOp(); linkAST(minus,jjtThis); operations.add(minus); }
-                [ <K_ALL> { minus.setAll(true); } | <K_DISTINCT> { minus.setDistinct(true); } [ (<K_BY> <K_NAME>) | (<K_STRICT> <K_CORRESPONDING>) ] ]
+                <K_MINUS> [ modifier=SetOperationModifier() ] { MinusOp minus = new MinusOp(); linkAST(minus,jjtThis); operations.add(minus); }
             )
             |
             (
-                <K_EXCEPT> { ExceptOp except = new ExceptOp(); linkAST(except,jjtThis); operations.add(except); }
-                [ <K_ALL> { except.setAll(true); } | <K_DISTINCT> { except.setDistinct(true); } [ (<K_BY> <K_NAME>) | (<K_STRICT> <K_CORRESPONDING>) ] ]
+                <K_EXCEPT> [ modifier=SetOperationModifier() ] { ExceptOp except = new ExceptOp(); linkAST(except,jjtThis); operations.add(except); }
             )
            
         )

src/test/java/net/sf/jsqlparser/statement/select/SelectTest.java
@@ -6407,4 +6407,15 @@ public class SelectTest {
                 parser -> parser.withDialect(Dialect.EXASOL));
     }
 
+    @Test
+    void testSQL2016CorrespondingBy() throws JSQLParserException {
+        String sqlStr =
+                "SELECT id, name, dept, salary\n" +
+                        "FROM Employees_US\n" +
+                        "UNION CORRESPONDING BY (id, name, dept)\n" +
+                        "SELECT dept, id, name, country\n" +
+                        "FROM Employees_EU;";
+
+        TestUtils.assertSqlCanBeParsedAndDeparsed(sqlStr, true);
+    }
 }

src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/union06.sql
@@ -45,3 +45,4 @@ order by 4,3,1
 
 --@SUCCESSFULLY_PARSED_AND_DEPARSED first on Aug 3, 2021, 7:20:08 AM
 --@FAILURE: Encountered unexpected token: "minus" "MINUS" recorded first on Feb 13, 2025, 10:16:06 AM
+--@FAILURE: ((select "x"."r_no","x"."i_id","x"."ind","x"."item",'0' "o" from "x" where("x"."r_no"=:a))union(select "y"."r_no","y"."i_id","y"."ind","y"."item",'0' "o" from "y" where("y"."r_no"=:a)))union((select "y"."r_no","y"."i_id","y"."ind","y"."item",'1' "o" from "y" where("y"."r_no"=:a))union(select "x"."r_no","x"."i_id","x"."ind","x"."item",'1' "o" from "x" where("x"."r_no"=:a)))order by 4,3,1 recorded first on Aug 21, 2025, 7:56:53 AM

commit bcc6de6afa14adb74688ec13963d43e688a7357f

fix: recognise set operation quantifiers written in any case
The grammar records a set operation's modifier verbatim from the source, so
`union all` arrives as "all" while `isAll()` asked whether the modifier
contained "ALL". Every spelling but upper case therefore read as an unqualified
UNION, silently dropping ALL semantics — and the same for DISTINCT, across
UNION, EXCEPT, INTERSECT and MINUS alike.

A regression from 5fe938bc, which replaced the boolean `all` and `distinct`
fields with a single verbatim modifier string. The round-trip tests all spell
their SQL in upper case, so none of them noticed.

Match the leading keyword case-insensitively instead. The match is anchored at
the start of the modifier and must end on a word boundary, so a
`CORRESPONDING BY NAME MATCHING(all)` column list is no longer mistaken for the
ALL quantifier the way a bare `contains` would.

`setAll` and `setDistinct` wrote their keyword whatever they were passed, so
`setAll(false)` left an ALL modifier behind. They now honour the argument.

src/main/java/net/sf/jsqlparser/statement/select/SetOperation.java
@@ -20,19 +20,35 @@ public abstract class SetOperation extends ASTNodeAccessImpl {
     }
 
     public boolean isAll() {
-        return modifier != null && modifier.contains("ALL");
+        return leadsWith("ALL");
     }
 
     public void setAll(boolean all) {
-        this.modifier = "ALL";
+        this.modifier = all ? "ALL" : "";
     }
 
     public boolean isDistinct() {
-        return modifier != null && modifier.contains("DISTINCT");
+        return leadsWith("DISTINCT");
     }
 
     public void setDistinct(boolean distinct) {
-        this.modifier = "DISTINCT";
+        this.modifier = distinct ? "DISTINCT" : "";
+    }
+
+    /**
+     * Whether the modifier opens with {@code keyword}, which the grammar records verbatim from the
+     * source and so may be written in any case. Anchored at the start and required to end on a word
+     * boundary, so a {@code CORRESPONDING BY NAME MATCHING(all)} column list cannot be mistaken for
+     * the ALL quantifier.
+     */
+    private boolean leadsWith(String keyword) {
+        if (modifier == null) {
+            return false;
+        }
+        String trimmed = modifier.trim();
+        return trimmed.regionMatches(true, 0, keyword, 0, keyword.length())
+                && (trimmed.length() == keyword.length()
+                        || !Character.isLetterOrDigit(trimmed.charAt(keyword.length())));
     }
 
     private final SetOperationType type;

src/test/java/net/sf/jsqlparser/statement/select/SetOperationModifierTest.java
@@ -16,6 +16,7 @@ import java.util.stream.Stream;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.Statement;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -48,6 +49,72 @@ public class SetOperationModifierTest {
     })
     void testSetOperationModifierRoundTrip(String sql) throws JSQLParserException {
         assertSqlCanBeParsedAndDeparsed(sql);
+    }
+
+    /**
+     * The grammar records the modifier verbatim from the source, so the quantifier survives in
+     * whatever case it was written. Every case spells the same set operation.
+     *
+     * @see <a href="https://github.com/JSQLParser/JSqlParser/issues/2419">#2419</a>
+     */
+    @ParameterizedTest
+    @ValueSource(strings = {"ALL", "all", "All", "aLL"})
+    void testAllModifierRecognisedInAnyCase(String quantifier) throws JSQLParserException {
+        SetOperation operation = firstOperation(
+                "SELECT a FROM t1 UNION " + quantifier + " SELECT a FROM t2");
+        assertTrue(operation.isAll(), "UNION " + quantifier + " should be an ALL union");
+        assertFalse(operation.isDistinct(), "UNION " + quantifier + " is not DISTINCT");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"DISTINCT", "distinct", "Distinct"})
+    void testDistinctModifierRecognisedInAnyCase(String quantifier) throws JSQLParserException {
+        SetOperation operation = firstOperation(
+                "SELECT a FROM t1 UNION " + quantifier + " SELECT a FROM t2");
+        assertTrue(operation.isDistinct(), "UNION " + quantifier + " should be a DISTINCT union");
+        assertFalse(operation.isAll(), "UNION " + quantifier + " is not ALL");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"EXCEPT", "INTERSECT", "MINUS"})
+    void testAllModifierRecognisedInAnyCaseForEverySetOperation(String setOperation)
+            throws JSQLParserException {
+        assertTrue(firstOperation("SELECT a FROM t1 " + setOperation + " all SELECT a FROM t2")
+                .isAll(), setOperation + " all should be an ALL operation");
+    }
+
+    /**
+     * A plain set operation carries no quantifier, and neither predicate may claim one.
+     */
+    @Test
+    void testUnqualifiedSetOperationIsNeitherAllNorDistinct() throws JSQLParserException {
+        SetOperation operation = firstOperation("SELECT a FROM t1 UNION SELECT a FROM t2");
+        assertFalse(operation.isAll());
+        assertFalse(operation.isDistinct());
+    }
+
+    /**
+     * The setters record what they are told rather than the keyword they are named after.
+     */
+    @Test
+    void testSettersHonourTheirArgument() {
+        UnionOp union = new UnionOp();
+
+        union.setAll(true);
+        assertTrue(union.isAll());
+        union.setAll(false);
+        assertFalse(union.isAll(), "setAll(false) must not leave an ALL modifier behind");
+
+        union.setDistinct(true);
+        assertTrue(union.isDistinct());
+        union.setDistinct(false);
+        assertFalse(union.isDistinct(), "setDistinct(false) must not leave a DISTINCT modifier");
+    }
+
+    private static SetOperation firstOperation(String sql) throws JSQLParserException {
+        Statement statement = CCJSqlParserUtil.parse(sql);
+        assertInstanceOf(SetOperationList.class, statement);
+        return ((SetOperationList) statement).getOperations().get(0);
     }
 
     @ParameterizedTest