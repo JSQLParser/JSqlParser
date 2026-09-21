6d7abaa95ffd89ec9d47c83bed67cbd1cb6b52ea
minleejae <mmj9808@gmail.com>
2026-09-21 23:19:08 +0900

    feat: add a structured SQL JSON predicate
    Signed-off-by: minleejae <mmj9808@gmail.com>

--- src/main/java/net/sf/jsqlparser/util/TablesNamesFinder.java
+++ src/main/java/net/sf/jsqlparser/util/TablesNamesFinder.java
@@ -75,6 +75,7 @@ import net.sf.jsqlparser.expression.operators.relational.IsBooleanExpression;
 import net.sf.jsqlparser.expression.operators.relational.IsDistinctExpression;
 import net.sf.jsqlparser.expression.operators.relational.IsNullExpression;
 import net.sf.jsqlparser.expression.operators.relational.IsUnknownExpression;
+import net.sf.jsqlparser.expression.operators.relational.IsJsonExpression;
 import net.sf.jsqlparser.expression.operators.relational.JsonOperator;
 import net.sf.jsqlparser.expression.operators.relational.LikeExpression;
 import net.sf.jsqlparser.expression.operators.relational.Matches;
@@ -718,6 +719,12 @@ public class TablesNamesFinder<Void>
     @Override
     public <S> Void visit(IsBooleanExpression isBooleanExpression, S context) {
         isBooleanExpression.getLeftExpression().accept(this, context);
+        return null;
+    }
+
+    @Override
+    public <S> Void visit(IsJsonExpression expression, S context) {
+        expression.getLeftExpression().accept(this, context);
         return null;
     }
 
