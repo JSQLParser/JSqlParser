95ad9088a56f36979a064c7fc0992ada054bf7e0
Jake Wang <jakezwang@users.noreply.github.com>
2026-09-24 05:48:26 -0400

    fix: default wildcard exclusions to EXCEPT (#2652)
    Signed-off-by: Jake Wang <jake.wang@argonlabs.tech>

--- src/test/java/net/sf/jsqlparser/statement/select/AllColumnsTest.java
+++ src/test/java/net/sf/jsqlparser/statement/select/AllColumnsTest.java
@@ -9,7 +9,17 @@
  */
 package net.sf.jsqlparser.statement.select;
 
+import static net.sf.jsqlparser.test.TestUtils.assertDeparse;
+import static net.sf.jsqlparser.test.TestUtils.assertSqlCanBeParsedAndDeparsed;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.ColumnsTransformer;
+import net.sf.jsqlparser.expression.ColumnsTransformer.ColumnsTransformerType;
+import net.sf.jsqlparser.expression.operators.relational.ParenthesedExpressionList;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.test.TestUtils;
 import org.junit.jupiter.api.Test;
 
@@ -30,5 +40,57 @@ class AllColumnsTest {
                 "SELECT * EXCLUDE(order_id) REPLACE(\"widget\" AS item_name), \"more\" as more_fields\n"
                         + "FROM orders";
         TestUtils.assertSqlCanBeParsedAndDeparsed(sqlStr, true);
+    }
+
+    @Test
+    void addExceptColumnToNewWildcard() throws JSQLParserException {
+        AllColumns columns = new AllColumns();
+        columns.addTransformer(new ColumnsTransformer(ColumnsTransformerType.EXCEPT)
+                .setExceptColumns(new ParenthesedExpressionList<>(new Column("hidden"))));
+        PlainSelect select = new PlainSelect().addSelectItem(columns).withFromItem(new Table("t"));
+
+        String expected = "SELECT * EXCEPT (hidden) FROM t";
+        assertEquals(expected, select.toString());
+        assertDeparse(select, expected);
+        assertSqlCanBeParsedAndDeparsed(expected);
+    }
+
+    @Test
+    void setExceptColumnsOnParsedWildcard() throws JSQLParserException {
+        PlainSelect select = (PlainSelect) CCJSqlParserUtil.parse("SELECT * FROM t");
+        AllColumns columns = select.getSelectItem(0).getExpression(AllColumns.class);
+        columns.addTransformer(new ColumnsTransformer(ColumnsTransformerType.EXCEPT)
+                .setExceptColumns(new ParenthesedExpressionList<>(new Column("hidden"))));
+
+        String expected = "SELECT * EXCEPT (hidden) FROM t";
+        assertEquals(expected, select.toString());
+        assertDeparse(select, expected);
+        assertSqlCanBeParsedAndDeparsed(expected);
+    }
+
+    @Test
+    void addExceptColumnToParsedTableWildcard() throws JSQLParserException {
+        PlainSelect select = (PlainSelect) CCJSqlParserUtil.parse("SELECT t.* FROM t");
+        AllTableColumns columns = select.getSelectItem(0).getExpression(AllTableColumns.class);
+        columns.addTransformer(new ColumnsTransformer(ColumnsTransformerType.EXCEPT)
+                .setExceptColumns(new ParenthesedExpressionList<>(new Column("hidden"))));
+
+        String expected = "SELECT t.* EXCEPT (hidden) FROM t";
+        assertEquals(expected, select.toString());
+        assertDeparse(select, expected);
+        assertSqlCanBeParsedAndDeparsed(expected);
+    }
+
+    @Test
+    void preserveExplicitExcludeTransformer() throws JSQLParserException {
+        AllColumns columns = new AllColumns();
+        columns.addTransformer(new ColumnsTransformer(ColumnsTransformerType.EXCLUDE)
+                .setExceptColumns(new ParenthesedExpressionList<>(new Column("hidden"))));
+        PlainSelect select = new PlainSelect().addSelectItem(columns).withFromItem(new Table("t"));
+
+        String expected = "SELECT * EXCLUDE (hidden) FROM t";
+        assertEquals(expected, select.toString());
+        assertDeparse(select, expected);
+        assertSqlCanBeParsedAndDeparsed(expected);
     }
 }
