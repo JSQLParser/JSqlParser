a1b82fcea871e9f0573397b8b1d21e046668eed3
Minjae Lee <mmj9808@gmail.com>
2026-09-24 22:43:27 +0900

    feat: share MySQL scalar table options across CREATE and ALTER (#2694)
    * feat: share MySQL scalar table options across CREATE and ALTER
    
    Signed-off-by: minleejae <mmj9808@gmail.com>
    
    * test: cover MySQL 9 vector boundaries with table options
    
    Signed-off-by: minleejae <mmj9808@gmail.com>
    
    ---------
    
    Signed-off-by: minleejae <mmj9808@gmail.com>

--- src/main/java/net/sf/jsqlparser/statement/alter/AlterExpressionTableOption.java
+++ src/main/java/net/sf/jsqlparser/statement/alter/AlterExpressionTableOption.java
@@ -9,6 +9,8 @@
  */
 package net.sf.jsqlparser.statement.alter;
 
+import net.sf.jsqlparser.statement.create.table.TableOption;
+
 /**
  * Internal subclass for table-level option operations within ALTER TABLE. Handles ENGINE,
  * ALGORITHM, LOCK, KEY_BLOCK_SIZE, COMMENT, ENCRYPTION, AUTO_INCREMENT (SET_TABLE_OPTION),
@@ -16,9 +18,52 @@ package net.sf.jsqlparser.statement.alter;
  */
 public class AlterExpressionTableOption extends AlterExpression {
 
+    private TableOption structuredTableOption;
+
+    public TableOption getStructuredTableOption() {
+        return structuredTableOption;
+    }
+
+    /** Uses the same option model as CREATE TABLE, with legacy getters as live projections. */
+    public void setStructuredTableOption(TableOption option) {
+        structuredTableOption = option;
+        setOperation(AlterOperation.SET_TABLE_OPTION);
+        super.setTableOption(null);
+        super.setUseEqual(false);
+    }
+
+    @Override
+    public String getTableOption() {
+        return structuredTableOption == null ? super.getTableOption()
+                : structuredTableOption.toString();
+    }
+
+    @Override
+    public void setTableOption(String option) {
+        structuredTableOption = null;
+        super.setTableOption(option);
+    }
+
+    @Override
+    public boolean getUseEqual() {
+        return structuredTableOption == null ? super.getUseEqual()
+                : structuredTableOption.isUseEquals();
+    }
+
+    @Override
+    public void setUseEqual(boolean useEqual) {
+        if (structuredTableOption != null) {
+            structuredTableOption.setUseEquals(useEqual);
+        }
+        super.setUseEqual(useEqual);
+    }
+
     @Override
     protected void appendBody(StringBuilder b) {
         switch (getOperation()) {
+            case SET_TABLE_OPTION:
+                b.append(getTableOption());
+                break;
             case COMMENT:
                 b.append("COMMENT ");
                 b.append(getCommentText());
